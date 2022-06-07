f56a220b1d074d778b9e6855b368c96b546da397
Sophia Leeman <sml96@cs.duke.edu>
2022-06-07 12:55:05 -0400

    Updated the sequence object to include the name of the sequence. Now, this code creates sequence objects with unique names, sequences, and dictionaries. I still need to make the dictionaries all share the same keyset

--- ReadingFasta.py
+++ ReadingFasta.py
@@ -1,12 +1,36 @@
 # Import featurize function
 from Kmerizing import *
 
-fasta = open("./outputDb_ss.fasta")
+#Creating sequence class
+class Seq:
+    def __init__(self, name, sequence, dictionary):
+        self.name = name
+        self.sequence = sequence
+        self.dictionary = dictionary
+    def __repr__(self):
+        return self.name
+
+#Initialize sequence array
+seqs = []
+
+#Read fasta file
+fasta = open("/home/users/sml96/bin/ProteinFoldRF/outputDb_ss.fasta")
+i = 0
+j = 0
 for line in fasta:
-    line = line.replace('\n','')
-    if line[0] != '>':
-        featurize(line) # At the moment, the code prints different dictionaries for each fasta sequence. 
+    if j%2 == 0:
+        name = line.replace('\n','')
+        seqs.append(Seq(name, '', featurize('')))
+        i += 1
+    else:
+        sequence = line.replace('\n','')
+        seqs[i-1].sequence = sequence
+        seqs[i-1].dictionary = featurize(sequence)
+    j +=1
+    
+
+        # At the moment, the code prints different dictionaries for each fasta sequence. 
         #TODO: Retain different dictionaries for each sequence in separate variables that contain the same keyset.
         # I will probably need to create a class so I can have sequence objects with dictionaries that get updated by the function.
         # The challenge will be keeping the keyset as small as it can be while making sure all of the sequences can use the same keyset.
-        
+ 
