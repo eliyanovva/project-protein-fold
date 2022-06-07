e168cf90ef9c293ebb2ffd007b3dfd5f4ce11a5f
Sophia Leeman <sml96@cs.duke.edu>
2022-06-07 11:36:51 -0400

    This code created a dictionary of all possible k-mers, but I realized that this is unnecessary, so I am going back to a previous commit after this

--- AllKmers.py
+++ AllKmers.py
@@ -1,25 +1,23 @@
-# Python 3 program to print all
-# possible strings of length k
-     
-# The method that prints all
-# possible strings of length k.
-# It is mainly a wrapper over
-# recursive function printAllKLengthRec()
-def printAllKLength(set, k):
- 
+# Python 3 program to create dictionary of k-mers
+
+# Wrapper over
+# recursive function createDictionaryRec()
+dict = {}
+
+def createDictionary(set, k):
     n = len(set)
-    printAllKLengthRec(set, "", n, k)
+    dict = createDictionaryRec(set, "", n, k)
  
 # The main recursive method
 # to print all possible
 # strings of length k
-def printAllKLengthRec(set, prefix, n, k):
-     
+def createDictionaryRec(set, prefix, n, k):
+    global dict
     # Base case: k is 0,
     # print prefix
     if (k == 0) :
-        print(prefix)
-        return
+        dict[prefix] = 0
+        return 
  
     # One by one add all characters
     # from set and recursively
@@ -31,14 +29,15 @@ def printAllKLengthRec(set, prefix, n, k):
             
             # k is decreased, because
             # we have added a new character
-            printAllKLengthRec(set, newPrefix, n, k - 1)
+            createDictionaryRec(set, newPrefix, n, k - 1)
  
 # Driver Code
 if __name__ == "__main__":
      
     set1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S' 'T', 'X']
     k = 5
-    printAllKLength(set1, k)
+    createDictionary(set1, k)
+    print(dict['LDPAW'])
  
 # This code is adapted from code by
 # ChitraNayal
--- Kmerizing.py
+++ Kmerizing.py
@@ -1,10 +1,20 @@
+#Imports
+# Need iter_kmers function
 from skbio import Sequence
+# Need createDictionary function
+from AllKmers import *
+
+#Input 3Di sequence here
 s = Sequence('DDDPLDPAWWFQEQADPDDLVVLVVVLVVLVVLLVLLCVLLVVLLVLCVPDPVCPDLLSLLSNLLSVLSNQLSVLQSVLLSCCSVPSPDIDTPVSQLVSQLSNQLSVLLNLLSLLVSLVLLLCCQVPVVCSCVVPDPVVSVVSSVVSNVVSNVLSVVLSVLLVPFRFDDRSYQHASGNHSVSSLVRTPDDCVVNVVSVVVSCCCRLVVSLVSSVVSVVSSVVSLVPDPDPVSNVVSCLSCVLSVVLSCLVNVLVCCLVPPDPPPDDPSVSNVSVVCSSSVSSSCNSVSVQVSDPVNVVSVVVVVVVVVVD')
-dict = {}
+
+#Initialize 3Di dictionary
+set1 = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S' 'T', 'X']
+k = 5
+createDictionary(set1, k)
+print(dict['DDDPL'])
+
 for kmer in s.iter_kmers(5, overlap=True):
      key = str(kmer)
-     if key not in dict:
-          dict[key] = 0
      dict[key] += 1
 print(dict)
      
