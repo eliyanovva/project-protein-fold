63c1b394c4c07d66db884fc6cf1d9780418fab82
Sophia Leeman <sml96@cs.duke.edu>
2022-06-08 14:39:19 -0400

    Fixed the ReadingFasta.py file to handle the newlines

--- RF/ReadingFasta.py
+++ RF/ReadingFasta.py
@@ -21,14 +21,14 @@ fasta = open("/home/users/sml96/bin/project-protein-fold/AminoAcidSequences/cate
 i = 0
 j = 0
 for line in fasta:
-    if j%2 == 0:
+    if line[0] == '>':
         name = line.replace('\n','')
         seqs.append(Seq(name, '', featurize('',5)))
         i += 1
     else:
         sequence = line.replace('\n','')
-        seqs[i-1].sequence = sequence
-        seqs[i-1].dictionary = featurize(sequence,5)
+        seqs[i-1].sequence = seqs[i-1].sequence + sequence
+        seqs[i-1].dictionary = featurize(seqs[i-1].sequence,5)
     j += 1
 
 #This prints all of the k-mers identified in the sequences
@@ -53,4 +53,5 @@ for seq in seqs:
 #Prints a matrix with columns corresponding to k-mers and rows corresponding to proteins
 #print(matrix)
 
-print(seqs)
+#Prints protein names
+#print(seqs)
