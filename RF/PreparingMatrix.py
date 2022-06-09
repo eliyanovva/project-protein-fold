a182e6e8427b2183815aa440fbee1f053ac8b80e
Sophia Leeman <sml96@cs.duke.edu>
2022-06-09 10:29:21 -0400

    Working on converting the csv into a dictionary matching protein and ligand to log FC

--- RF/PreparingMatrix.py
+++ RF/PreparingMatrix.py
@@ -1,4 +1,9 @@
 import ReadingFasta
+import pandas as pd
 
 #Load in the matrices
 ReadingFasta.import_variables()
+
+#Convert csv to dictionary
+dict = {}
+data = pd.read_csv('/home/users/sml96/bin/project-protein-fold/olfr_de/uniprot_ligand_logfc.csv')
