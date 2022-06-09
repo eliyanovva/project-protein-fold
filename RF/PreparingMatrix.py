173b05321b79d29af4f2312150b7577aaec6b23e
Sophia Leeman <sml96@cs.duke.edu>
2022-06-09 11:37:27 -0400

    Created dictionary of protein/ligand pair keys with logFC values

--- RF/PreparingMatrix.py
+++ RF/PreparingMatrix.py
@@ -6,4 +6,9 @@ ReadingFasta.import_variables()
 
 #Convert csv to dictionary
 dict = {}
-data = pd.read_csv('/home/users/sml96/bin/project-protein-fold/olfr_de/uniprot_ligand_logfc.csv')
+df = pd.read_csv('/home/users/sml96/bin/project-protein-fold/olfr_de/uniprot_ligand_logfc.csv', header = None)
+
+for i in range(0, 35477, 2):
+    dict[frozenset(df.loc[i])] = df.loc[i+1][1]
+
+print(dict)
