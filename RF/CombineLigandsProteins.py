f76b09d4ef3c8b0cdf28bd1e8b3d654dd9de3a99
Sophia Leeman <sml96@cs.duke.edu>
2022-06-10 09:53:07 -0400

    Incorporating ligand data into the CombineLigansProteins file

--- RF/CombineLigandsProteins.py
+++ RF/CombineLigandsProteins.py
@@ -1,5 +1,9 @@
 import PreparingMatrix
+import SmileKmer
+
 
 #Import proteins matrix
 PreparingMatrix.access_matrix()
 proteins_matrix = PreparingMatrix.intermediate_matrix
+
+print(ligand_matrix())
