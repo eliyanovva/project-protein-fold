9ddaa52109e99e7c0c2642468a387ff77d23ecae
MollyB-creator <57322435+MollyB-creator@users.noreply.github.com>
2022-06-10 14:22:09 -0400

    cast freq_mat into a list

--- RF/SmileKmer.py
+++ RF/SmileKmer.py
@@ -27,8 +27,8 @@ def ligand_matrix(ligands, k, num_proteins):
     freq_mat = []
     for i in range(num_proteins):
         for lig in ligand_counts:
-            freq_mat.append(np.array(ligand_counts[lig].values()))
-            print(np.array(ligand_counts[lig].values()))
+            freq_mat.append(list(ligand_counts[lig].values()))
+            print(list(ligand_counts[lig].values()))
    
     return np.matrix(freq_mat)
 
