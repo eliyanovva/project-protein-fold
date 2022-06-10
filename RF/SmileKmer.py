6cc34874c53f49be9a4ca6d3365407b9230bc0d7
MollyB-creator <57322435+MollyB-creator@users.noreply.github.com>
2022-06-10 13:59:31 -0400

    updated the order of the functions

--- RF/SmileKmer.py
+++ RF/SmileKmer.py
@@ -1,6 +1,5 @@
 import numpy as np
 
-
 ligand_dict = {'2hepatanone': 'CCCCCC(=O)C','2hexanone': 'CCCCC(=O)C','3methyl1butanethiol': 'CC(C)CCS',
 'acetophenone': 'CC(=O)C1=CC=CC=C1','aPinene': 'CC1=CCC2CC1C2(C)C','bCaryophyllene': 'CC1=CCCC(=C)C2CC(C2CC1)(C)C',
 'bDamascone': 'CC=CC(=O)C1=C(CCCC1(C)C)C','Benzaldehyde': 'C1=CC=C(C=C1)C=O','bIonone': 'CC1=C(C(CCC1)(C)C)C=CC(=O)C',
@@ -12,7 +11,72 @@ ligand_dict = {'2hepatanone': 'CCCCCC(=O)C','2hexanone': 'CCCCC(=O)C','3methyl1b
 'propionicAcid': 'CCC(=O)O','pyridine': 'C1=CC=NC=C1','tbm': 'CC(C)(C)S','transCinnamaldehyde': 'C1=CC=C(C=C1)C=CC=O',
 'androstenone': 'CC12CCC3C(C1CC=C2)CCC4C3(CCC(=O)C4)C','ebFarnesene': 'CC(=CCCC(=CCCC(=C)C=C)C)C','2proplythietane': 'CCCC1CCS1',
 'citral': 'CC(=CCCC(=CC=O)C)C','cyclopentanethiol': 'C1CCC(C1)S','e2butene1thiol': 'CC=CCS','isovalericAcid': 'CC(C)CC(=O)O'}
+        
+def importmatrix():
+    global ligmat
+    ligmat = ligand_matrix(ligand_dict, 5, 232)
 
+def ligand_matrix(ligands, k, num_proteins):
+    ligand_counts = ligand_kmer_count(ligands, k)
+    freq_mat = []
+    for i in range(num_proteins):
+        for lig in ligand_counts:
+            freq_mat.append((ligand_counts[lig].values()))
+    return np.matrix(freq_mat)
+
+def ligand_kmer_count(ligands, k):
+    ligand_counts = {}
+    total_kmers = find_total_kmers(ligands, k)
+    for lig in ligands:
+        lig_dict = {}
+        for kmer in total_kmers:
+            lig_dict[kmer] = 0
+        freq_dict = smile_dict(ligands[lig], k)
+        for kster in freq_dict:
+            lig_dict[kster] = freq_dict[kster]
+        ligand_counts[lig] = lig_dict
+    return ligand_counts
+
+def find_total_kmers(ligands, k):
+    kmers = []
+    for lig in ligands:
+        k_list = smile_list(ligands[lig], k)
+        for kmer in k_list:
+            if kmers.count(kmer) == 0:
+                kmers.append(kmer)
+    return kmers
+
+def smile_dict(smile, k):
+    #Option 1 ~  Characters are:
+    #atoms in the background
+    #the entirety of any side chains
+    #double bonds
+    kmer_dict = {}
+    letters = form_letters(smile)
+
+    for i in range(0, len(letters) - k + 1):
+        k_ster = ""
+        for j in range(k):
+            k_ster += letters[i+j]
+        if k_ster not in kmer_dict:
+            kmer_dict[k_ster] = 0
+        kmer_dict[k_ster] += 1
+
+    return kmer_dict
+
+def smile_list(smile, k):
+    kmer_list = []
+    letters = form_letters(smile)
+
+    for i in range(0, len(letters) - k + 1):
+        k_ster = ""
+        for j in range(k):
+            k_ster += letters[i+j]
+        if kmer_list.count(k_ster) == 0:
+            kmer_list.append(k_ster)
+            
+    return kmer_list
+        
 def form_letters(smile):
     letters = []  # list that stores the sectioned off 'letters' of the str smile
     for i in range(0, len(smile)):
@@ -72,76 +136,6 @@ def form_letters(smile):
 
     return letters
 
-def smile_list(smile, k):
-    kmer_list = []
-    letters = form_letters(smile)
-
-    for i in range(0, len(letters) - k + 1):
-        k_ster = ""
-        for j in range(k):
-            k_ster += letters[i+j]
-        if kmer_list.count(k_ster) == 0:
-            kmer_list.append(k_ster)
-
-    return kmer_list
-
-def smile_dict(smile, k):
-    #Option 1 ~  Characters are:
-    #atoms in the background
-    #the entirety of any side chains
-    #double bonds
-    kmer_dict = {}
-    letters = form_letters(smile)
-
-    for i in range(0, len(letters) - k + 1):
-        k_ster = ""
-        for j in range(k):
-            k_ster += letters[i+j]
-        if k_ster not in kmer_dict:
-            kmer_dict[k_ster] = 0
-        kmer_dict[k_ster] += 1
-
-    return kmer_dict
-
-def check_kmers(ligands):
-    f_chart = open('kmer_counts.txt', 'w')
-    f_chart.write("k" + "\t" + "num of k-mers" +  "\t" +  "num. freq = 1" + "\t" + "\t" +  "prop. freq = 1\n")
-
-    for i in range(2, 11):
-        for item in ligands:
-            dict = smile_dict(ligands[item], i)
-
-        freq_1 = 0
-        for item in dict:
-            if dict[item] == 1:
-                freq_1+= 1
-        prop = float(freq_1) / float(len(dict))
-
-        f_chart.write(str(i) + "\t" + "\t" + str(len(dict)) + "\t" + "\t" + "\t" + "\t" + str(freq_1) + "\t" + "\t" + "\t" + "\t" + str(prop) + '\n')
-    f_chart.close()
-
-def ligand_kmer_count(ligands, k):
-    ligand_counts = {}
-    total_kmers = find_total_kmers(ligands, k)
-    for lig in ligands:
-        lig_dict = {}
-        for kmer in total_kmers:
-            lig_dict[kmer] = 0
-        freq_dict = smile_dict(ligands[lig], k)
-        for kster in freq_dict:
-            lig_dict[kster] = freq_dict[kster]
-        ligand_counts[lig] = lig_dict
-    return ligand_counts
-
-def find_total_kmers(ligands, k):
-    kmers = []
-    for lig in ligands:
-        k_list = smile_list(ligands[lig], k)
-        for kmer in k_list:
-            if kmers.count(kmer) == 0:
-                kmers.append(kmer)
-    return kmers
-
 def check_ligand_distinct(ligands, k):
     num_ligands = len(ligands)
     freq_mat = []
@@ -157,15 +151,3 @@ def check_ligand_distinct(ligands, k):
         print('Ligands are distinct')
     else:
         print('Ligands are not distinct')
-
-def ligand_matrix(ligands, k, num_proteins):
-    ligand_counts = ligand_kmer_count(ligands, k)
-    freq_mat = []
-    for i in range(num_proteins):
-        for lig in ligand_counts:
-            freq_mat.append((ligand_counts[lig].values()))
-    return np.matrix(freq_mat)
-
-def importmatrix():
-    global ligmat
-    ligmat = ligand_matrix(ligand_dict, 5, 232)
