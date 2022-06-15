5287a1206973e0db04d6173629b1eaf9ea4adb5a
Sophia Leeman <sml96@cs.duke.edu>
2022-06-15 16:20:37 -0400

    tried a log relationship instead, which worked much better

--- CorrectingLogFC.py
+++ CorrectingLogFC.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
 
 data = pd.read_csv('e3fp/pS6_DE_1p_2heptanone.csv')
 y = np.abs(data.loc[:,["logFC"]].values)
@@ -9,13 +10,14 @@ x = (data.loc[:,["No1"]].values + data.loc[:,["No2"]].values + data.loc[:,["No3"
 testx = x.flatten()
 testy = y.flatten()
 
-model = np.polyfit(testx, testy, 1)
+params = np.array([1,1])
 
-plt.plot(testx, testy, 'bs')
-plt.plot(model[0] + model[1]*testx, 'rs')
-print(model)
+def funcinv(x, a, b):
+    x+=.00000000001
+    return b + a * np.log(x)
 
-rss = 0
-for i in range(0, len(y), 1):
-    rss += (testy[i] - (model[0] + model[1]*testx[i]))*(testy[i] - (model[0] + model[1]*testx[i]))
-print(rss)
+res = curve_fit(funcinv, testx, testy, params, maxfev = 1000000)
+print(res)
+
+plt.plot(testx,testy, 'rs')
+plt.plot(testx, -0.04010694 * np.log(testx) + 0.66274938 - .00000000001, 'bs')
