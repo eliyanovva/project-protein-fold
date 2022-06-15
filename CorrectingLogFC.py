77d213c43de18ccd26f6671724beb0b38ff947d6
Sophia Leeman <sml96@cs.duke.edu>
2022-06-15 16:38:31 -0400

    This script changes the data to make sure that none of the logFC values are influenced by abundance. However, it seems to drive everything toward zero except for ones with very low abundance. This does not seem like a great model

--- CorrectingLogFC.py
+++ CorrectingLogFC.py
@@ -19,5 +19,12 @@ def funcinv(x, a, b):
 res = curve_fit(funcinv, testx, testy, params, maxfev = 1000000)
 print(res)
 
-plt.plot(testx,testy, 'rs')
-plt.plot(testx, -0.04010694 * np.log(testx) + 0.66274938 - .00000000001, 'bs')
+#plt.plot(testx,testy, 'rs')
+#plt.plot(testx, -0.04010694 * np.log(testx) + 0.66274938 - .00000000001, 'bs')
+
+rss = 0
+for i in range(0, len(testy), 1):
+    rss += (testy[i] - (-0.04010694 * np.log(testx[i]) + 0.66274938))*(testy[i] - (-0.04010694 * np.log(testx[i]) + 0.66274938 - .00000000001))
+print(rss)
+
+plt.plot(testx, data.loc[:,["logFC"]].values.flatten()/(-0.04010694 * np.log(testx+.00000000001)), 'rs')
