6732e7eed6a39dc1c53bd0b74e0894e2ce861fdb
Sophia Leeman <sml96@cs.duke.edu>
2022-06-15 15:58:48 -0400

    Performed polyfit on the data to find relationship between abundance and logFC

--- CorrectingLogFC.py
+++ CorrectingLogFC.py
@@ -3,12 +3,19 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 import matplotlib.pyplot as plt
 
-data = pd.read_csv('Ligands_withSMILE/pS6_DE_1p_2heptanone.csv')
-y = np.abs(np.array(data.loc[:,["logFC"]]))
-x = (np.array(data.loc[:,["No1"]]) + np.array(data.loc[:,["No2"]]) + np.array(data.loc[:,["No3"]]))/3
+data = pd.read_csv('e3fp/pS6_DE_1p_2heptanone.csv')
+y = np.abs(data.loc[:,["logFC"]].values)
+x = (data.loc[:,["No1"]].values + data.loc[:,["No2"]].values + data.loc[:,["No3"]].values)/3
+testx = x.flatten()
+testy = y.flatten()
 
-model = np.polyfit(x, y)
-r_sq = model.score(x, y)
-print(f"coefficient of determination: {r_sq}")
+model = np.polyfit(testx, testy, 3)
 
-plt.plot(x, y)
+plt.plot(testx, testy, 'bs')
+plt.plot(model[0] + model[1]*testx + model[2]*testx*testx + model[3]*testx*testx*testx, 'rs')
+print(model)
+
+rss = 0
+for i in range(0, len(y), 1):
+    rss += (testy[i] - (model[0] + model[1]*testx[i] + model[2]*testx[i]*testx[i] + model[3]*testx[i]*testx[i]*testx[i]))*(testy[i] - (model[0] + model[1]*testx[i] + model[2]*testx[i]*testx[i] + model[3]*testx[i]*testx[i]*testx[i]))
+print(rss)
