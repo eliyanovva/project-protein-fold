5b8d6dda8bcd47ce8f4fd6ffb057a693add83e07
Sophia Leeman <sml96@cs.duke.edu>
2022-06-15 16:02:53 -0400

    Residual Sum of Squares better for linear model and still not good enough

--- CorrectingLogFC.py
+++ CorrectingLogFC.py
@@ -9,13 +9,13 @@ x = (data.loc[:,["No1"]].values + data.loc[:,["No2"]].values + data.loc[:,["No3"
 testx = x.flatten()
 testy = y.flatten()
 
-model = np.polyfit(testx, testy, 3)
+model = np.polyfit(testx, testy, 1)
 
 plt.plot(testx, testy, 'bs')
-plt.plot(model[0] + model[1]*testx + model[2]*testx*testx + model[3]*testx*testx*testx, 'rs')
+plt.plot(model[0] + model[1]*testx, 'rs')
 print(model)
 
 rss = 0
 for i in range(0, len(y), 1):
-    rss += (testy[i] - (model[0] + model[1]*testx[i] + model[2]*testx[i]*testx[i] + model[3]*testx[i]*testx[i]*testx[i]))*(testy[i] - (model[0] + model[1]*testx[i] + model[2]*testx[i]*testx[i] + model[3]*testx[i]*testx[i]*testx[i]))
+    rss += (testy[i] - (model[0] + model[1]*testx[i]))*(testy[i] - (model[0] + model[1]*testx[i]))
 print(rss)
