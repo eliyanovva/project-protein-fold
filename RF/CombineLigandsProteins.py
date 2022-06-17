e0a1fceb1ff70ec72200a7d7218a62fc62033209
Sophia Leeman <sml96@cs.duke.edu>
2022-06-17 12:44:40 -0400

    Trying oversampling: WARNING I have not determined a way to keep the copies of some of these from ending up in the test set. Still working on that

--- RF/CombineLigandsProteins.py
+++ RF/CombineLigandsProteins.py
@@ -11,7 +11,7 @@ import SMILE
 ligand_dict = SMILE.create_ligand_dict()
 cit_logFC, cit_pval, cit_corrected = labels.cit_labels()
 logFC, pVal = labels.labels()
-
+classified = labels.classified_logFC_pVal()
 
 def exportdicts():
     global citlog  
@@ -24,6 +24,8 @@ def exportdicts():
     logdic = logFC
     global pdic
     pdic = pVal
+    global class_dict
+    class_dict = classified
 
 #Import proteins matrix
 PreparingMatrix.access_matrix()
@@ -41,9 +43,15 @@ final_matrix = np.concatenate((proteins_matrix, ligand_matrix), axis = 1)
 ReadingFasta.import_variables()
 proteins = ReadingFasta.sequence_seqs
 logFCmat = []
+i = 0
 for protein in proteins:
     for ligand in list(ligand_dict.keys()):
-        logFCmat.append(float(logFC[str(protein.name)][ligand]))
+        logFCmat.append(float(classified[str(protein.name)][ligand]))
+        i+=1
+        if (float(classified[str(protein.name)][ligand]) == 1.0):
+            np.concatenate((final_matrix, np.reshape(final_matrix[i], (1,-1))), axis = 0)
+            proteins.append(protein)
+print(logFCmat[230:])
 
 
 def import_final():
