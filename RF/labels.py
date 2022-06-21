e9e59df1027b188b417af4e35f61f9ef86761f76
MollyB-creator <57322435+MollyB-creator@users.noreply.github.com>
2022-06-21 09:11:58 -0400

    Update labels.py

--- RF/labels.py
+++ RF/labels.py
@@ -1,15 +1,18 @@
 import pandas as pd
+import numpy as np
+import SMILE
 
 acc_ids = SMILE.create_protein_list()
 csvs = SMILE.create_ligand_list()
 
 fas_df = pd.read_csv('fasta_list.csv', index_col='accession number')
 
+num_proteins = len(acc_ids)
+num_ligands = len(csvs)
 logFC_byID = {}
 pVal_byID = {}
 cit_logFC = {}
 cit_pVal = {}
-cit_corrected = {}
 
 for id in acc_ids:
            logFC_byID[id] = {}
@@ -22,44 +25,46 @@ def cit_labels():
                       name = fas_df.loc[id]['receptor']
                       cit_logFC[id] = (cit_df.loc[name]['logFC'])
                       cit_pVal[id] = (cit_df.loc[name]['PValue'])
-                      cit_corrected[id] = (cit_df.loc[name]['logFC']*cit_df.loc[name]['No1']*cit_df.loc[name]['No2']*cit_df.loc[name]['No3']/3)
-           return cit_logFC, cit_pVal, cit_corrected
+           return cit_logFC, cit_pVal
 
 def labels():
            for csv in csvs:
                       file_name = 'olfr_de_copy1/olfr_de/'+csv
                       curr_df = pd.read_csv(file_name, index_col='name')
+
                       for id in acc_ids:
                                  name = fas_df.loc[id]['receptor']
                                  logFC_byID[id][csv] = (curr_df.loc[name]['logFC'])
                                  pVal_byID[id][csv] = (curr_df.loc[name]['PValue'])
            return logFC_byID, pVal_byID
 
-def classified_logFC():
-    logFC_byID, pVal_byID = labels()
+#cutoff for p-Val: > .05
+#cutoff for logFC: < .5
+#1 = yes, 0 = no
 
-    classified = {}
+def classified_logFC(logFC_byID, pVal_byID):
+    classified = np.zeros_like(np.arange(num_proteins * num_ligands))
+    i = 0
     for id in logFC_byID:
-        classified[id] = {}
         for csv in csvs:
             if logFC_byID[id][csv] < .5:
-                classified[id][csv] = 0
+                classified[i] = 0
             else:
-                classified[id][csv] = 1
+                classified[i] = 1
+            i += 1
 
     return classified
 
-def classified_pVal():
-    logFC_byID, pVal_byID = labels()
-
-    classified = {}
+def classified_pVal(logFC_byID, pVal_byID):
+    classified = np.zeros_like(np.arange(num_proteins * num_ligands))
+    i = 0
     for id in pVal_byID:
-        classified[id] = {}
         for csv in csvs:
             if pVal_byID[id][csv] > .05:
-                classified[id][csv] = 0
+                classified[i] = 0
             else:
-                classified[id][csv] = 1
+                classified[i] = 1
+            i += 1
 
     return classified
 
