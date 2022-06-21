cd53961c89fbe3dc27e08f868098b4fdcb4db67b
Sophia Leeman <sml96@cs.duke.edu>
2022-06-21 15:25:07 -0400

    Chose Randomized Search for now

--- RF/OptimizingHyperparameters.py
+++ RF/OptimizingHyperparameters.py
@@ -4,10 +4,28 @@
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.model_selection import RandomizedSearchCV
 
 #Number of trees in the forest
-n_estimators = [int(x) for x in np.linspace(10, 100, 10)]
+n_estimators = [int(x) for x in np.linspace(start = 10, stop = 100, num = 10)]
 #Features considered at each split
 max_features = ['auto', 'sqrt']
 #Number of levels in each tree
-max_depth = [int(x) for x in np.linspace(2, 10, 2)]
+max_depth = [int(x) for x in np.linspace(start = 2, stop = 10, num = 5)]
+#Samples required to split a node
+min_samples_split = [int(x) for x in np.linspace(start = 10, stop = 100, num = 10)]
+#Minimum samples at each leaf
+min_samples_leaf = [1,2]
+#Method of selecting samples for each tree
+bootstrap = [True, False]
+
+param_grid = {'n_estimators': n_estimators,
+                'max_features' : max_features,
+                'max_depth' : max_depth,
+                'min_samples_split' : min_samples_split,
+                'min_samples_leaf' : min_samples_leaf,
+                'bootstrap' : bootstrap}
+
+model = RandomForestClassifier()
+
+grid = RandomizedSearchCV(estimator = model, param_distributions = param_grid, cv = 10, verbose = 2, n_jobs = -1)
