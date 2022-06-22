1ac61424cf35663f24bd45667d3fee3107f69898
Sophia Leeman <sml96@cs.duke.edu>
2022-06-22 10:43:07 -0400

    Briefly commented out the filtering function and updated the number of proteins

--- RF/CombineLigandsProteins.py
+++ RF/CombineLigandsProteins.py
@@ -21,12 +21,6 @@ def import_labels():
     negatives = neg_counts
 
 def exportdicts():
-    global citlog
-    citlog = cit_logFC
-    global citp
-    citp = cit_pval
-    #global citcor
-    #citcor = cit_corrected
     global logdic
     logdic = logFC
     global pdic
@@ -44,9 +38,10 @@ fasta1 = open("/home/users/sml96/bin/project-protein-fold/AminoAcidSequences/all
 #Create kmer frequency dictionary
 seqvar1, features1 = ReadingFasta.make_seqvar(fasta1, Globals.categorized_seqs, Globals.categorized_features)
 #Remove insignificant kmers
-filter_feat = Filtering.richness_protein(features1, seqvar1, pos_counts, neg_counts)
+#filter_feat = Filtering.richness_protein(features1, seqvar1, pos_counts, neg_counts)
 # Make the matrix
-AA_mat = ReadingFasta.makematrix(seqvar1, filter_feat, Globals.categorized_matrix)
+#AA_mat = ReadingFasta.makematrix(seqvar1, filter_feat, Globals.categorized_matrix)
+AA_mat = ReadingFasta.makematrix(seqvar1, features1, Globals.categorized_matrix)
 
 #Creating output for 3Di sequences
 # Read fasta file
@@ -54,16 +49,17 @@ fasta2 = open("/home/users/sml96/bin/project-protein-fold/3DiSequences/fullset_s
 #Create kmer frequency dictionary
 seqvar2, features2 = ReadingFasta.make_seqvar(fasta2, Globals.di_seqs, Globals.di_features)
 #Remove insignificant kmers
-filter_feat2 = Filtering.richness_protein(features2, seqvar2, pos_counts, neg_counts)
+#filter_feat2 = Filtering.richness_protein(features2, seqvar2, pos_counts, neg_counts)
 # Make the matrix
-Di_mat = ReadingFasta.makematrix(seqvar2, filter_feat, Globals.di_matrix)
+#Di_mat = ReadingFasta.makematrix(seqvar2, filter_feat2, Globals.di_matrix)
+Di_mat = ReadingFasta.makematrix(seqvar2, features2, Globals.di_matrix)
 
 intermed_matrix = np.concatenate((np.array(AA_mat, dtype = np.uint8), np.array(Di_mat, dtype = np.uint8)) , axis = 1)
 ligand_count = 38
 proteins_matrix = np.repeat(intermed_matrix, repeats = ligand_count, axis = 0)
 
 #Import ligands matrix
-SmileKmer.importmatrix(ligand_dict, 5, 230)
+SmileKmer.importmatrix(ligand_dict, 5, 1084)
 ligand_matrix = SmileKmer.ligmat
 
 #Concatenate protein and ligand matrices
