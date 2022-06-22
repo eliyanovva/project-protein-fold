cf964a9f3eb279d0c20b2b9c010f4e3e1a999390
Teodora Petkova <teodorapetkovax@gmail.com>
2022-06-22 13:38:01 -0400

    generated bgf agjacency and feature matrices

--- graph_cnn/data_prep/data_generator.py
+++ graph_cnn/data_prep/data_generator.py
@@ -1,20 +1,27 @@
 import os
-from pdb_file_prep import PDB_Datafile
+from bgf_file_prep import BGFDataFile
 from mol_file_prep import MolDataFile
-def generatePDBAdjacencyMatrices(folder_path):
+def generateBGFAdjacencyMatrices(folder_path):
     for filename in os.listdir(folder_path):
-        # TODO: add regex check to ensure it is a pdb file
-        if filename.endswith('.pdb'):
-            pdb_file = PDB_Datafile(folder_path + filename)
+        if filename.endswith('.bgf'):
+            pdb_file = BGFDataFile(os.path.join(folder_path, filename))
             pdb_file.getAdjacencyMatrix()
+
+
+def generateBGFFeatureMatrices(folder_path):
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.bgf'):
+            bgf_file = BGFDataFile(os.path.join(folder_path, filename))
+            bgf_file.getFeatureMatrix()
 
 
 def generateMolAdjacencyMatrices(folder_path):
     for filename in os.listdir(folder_path):
         if filename.endswith('.mol'):
-            mol_file = MolDataFile(folder_path + filename)
+            mol_file = MolDataFile(os.path.join(folder_path, filename))
             mol_file.getAdjacencyMatrix()
 
 
-generateMolAdjacencyMatrices('/home/users/tep18/new_ppp/project-protein-fold/mol_files/')
-#generatePDBAdjacencyMatrices('/home/users/tep18/new_ppp/project-protein-fold/pdb_data_files/')
+#generateMolAdjacencyMatrices('/home/users/tep18/new_ppp/project-protein-fold/mol_files/')
+generateBGFAdjacencyMatrices('/home/users/tep18/new_ppp/project-protein-fold/bgf_files/')
+generateBGFFeatureMatrices('/home/users/tep18/new_ppp/project-protein-fold/bgf_files/')
