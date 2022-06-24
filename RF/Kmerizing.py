5bda7cf66d5b9c7e6a2c99720df188d20edb4e95
Sophia Leeman <sml96@cs.duke.edu>
2022-06-24 13:58:38 -0400

    Continuing Cleaning

--- RF/Kmerizing.py
+++ RF/Kmerizing.py
@@ -1,16 +1,5 @@
-#from skbio import Sequence
-"""
-def featurize(seq,k,feat):
-     s = Sequence(seq)
-     dict = {}
-     for kmer in s.iter_kmers(k, overlap=True):
-          key = str(kmer)
-          if key not in dict:
-               dict[key] = 0
-               feat.add(key)
-          dict[key] += 1
-     return dict
-"""
+#This script contains a function to extract kmers and kmer-frequency from protein sequences
+
 def featurize(seq,k,feat):
      dict = {}
      for i in range(0, len(seq) - k + 1):
--- RF/labels.py
+++ RF/labels.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import numpy as np
 import Globals
 
 acc_ids = Globals.initialize_protein_list()
