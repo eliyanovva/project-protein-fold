55c5e278de34846493ee007fce3be2352dc89e5e
Sophia Leeman <sml96@cs.duke.edu>
2022-06-27 16:15:48 -0400

    Using precision recall curve

--- RF/FixedClassificationModel.py
+++ RF/FixedClassificationModel.py
@@ -5,7 +5,7 @@
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn import metrics
-from imblearn.over_sampling import RandomOverSampler
+from imblearn.over_sampling import ADASYN
 
 def train(features, labels):
     #define features and labels
@@ -16,7 +16,7 @@ def train(features, labels):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1) # 90% training and 10% test
 
     #Oversampling was necessary, because most ligand/receptor pairs do not bind in our dataset
-    ros = RandomOverSampler()
+    ros = ADASYN()
 
     X_res, y_res = ros.fit_resample(X_train, y_train)
 
@@ -27,9 +27,11 @@ def train(features, labels):
     clf.fit(X_res,y_res)
 
     #Form predictions
-    y_pred=clf.predict_proba(X_test)
+    y_pred=clf.predict_proba(X_test)[:,1]
+
+    precision, recall, thresholds = metrics.precision_recall_curve(y_test, y_pred)
 
     #Print accuracy of the model
     print("Accuracy:",metrics.roc_auc_score(y_test, y_pred))
-    print("Accuracy:",metrics.f1_score(y_test, y_pred))
+    print("Accuracy:",metrics.auc(recall,precision))
 
