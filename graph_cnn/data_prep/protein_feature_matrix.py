b4bbc4e25d06fa9fe1aeeeb999db020435527b11
austin <austin@austins-MacBook-Air.local>
2022-06-29 16:58:00 -0400

    building protein feature matrix

---
+++ graph_cnn/data_prep/protein_feature_matrix.py
@@ -0,0 +1,63 @@
+import openbabel
+from openbabel import pybel
+import constants
+
+#importing protein
+filepath = "/Users/austin/Documents/ProteinFoldWork/project-protein-fold/data_files/pdb_data_files/AF-A0A0B4J1M2-F1-model_v2.pdb"
+prefile = next(pybel.readfile("pdb", filepath))
+
+#creating a Molecule object
+protein = pybel.Molecule(prefile)
+
+#building the feature matrix
+# rows are features, columns are the atoms in the molecule
+#using all the features - will figure out which are impactful later
+feature_matrix = []
+for x in protein:
+    row = []
+    atomic_mass = x.atomicmass
+    row.append(atomic_mass)
+    #coordinateidx - possible to do coordinate index,
+    #molecules are not lined up, so I am not adding it
+    #plus location is represented in the adjacency matrix
+    #notably other location based features are present,
+    #similarly I am not adding them
+    exact_mass = x.exactmass
+    row.append(exact_mass)
+    #unsure how this is different than atomic mass,
+    #will look into it
+    formal_charge = x.formalcharge
+    row.append(formal_charge)
+    heavy_degree = x.heavydegree
+    row.append(heavy_degree)
+    #number of non-hydrogen atoms attached
+    hetero_degree = x.heterodegree
+    row.append(hetero_degree)
+    #number of heteroatoms(non Carbons or Hydrogens) attached
+    hybridization = x.hyb
+    row.append(hybridization)
+    sequence = x.idx
+    row.append(sequence)
+    #interestingly/horrifyingly the index starts at one
+    isotope = x.isotope
+    row.append(isotope)
+    #isotope for the atom, 0 otherwise
+    partial_charge = x.partialcharge
+    row.append(partial_charge)
+    spin = x.spin
+    row.append(spin)
+    #spin multiplicity - complex orientation thing
+    #link if curious to learn more
+    #https://chem.libretexts.org/Bookshelves/Physical_and_Theoretical_Chemistry_Textbook_Maps/Supplemental_Modules_(Physical_and_Theoretical_Chemistry)/Electronic_Structure_of_Atoms_and_Molecules/Evaluating_Spin_Multiplicity
+    type = x.type
+    row.append(type)
+    #atom type
+    degree = x.degree
+    row.append(degree)
+    #number of explicit connections
+    feature_matrix.append(row)
+
+
+#Notes:
+# feature ideas - in binding pocket(a boolean)
+#use help(protein) to learn about the molecule object
