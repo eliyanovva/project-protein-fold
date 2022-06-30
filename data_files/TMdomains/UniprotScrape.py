f83a12bcecbeb44c1a778a3d926e9316615e8a54
Sophia Leeman <sml96@cs.duke.edu>
2022-06-30 15:00:00 -0400

    All TM domains are now stored in the proper format in TM.txt

--- data_files/TMdomains/UniprotScrape.py
+++ data_files/TMdomains/UniprotScrape.py
@@ -42,8 +42,8 @@ with open("TM.txt", "w") as f:
     #print(json.dumps(TM_dict, indent=4))
     for accession in TM_dict:
       if len(accession["features"]) > 6:
-        print(accession['accession'], file=f)
-        print(accession['sequence'][int(accession["features"][2]['begin'])-1:int(accession["features"][2]['end'])], file=f)
-        print(accession['sequence'][int(accession["features"][4]['begin'])-1:int(accession["features"][4]['end'])], file=f)
-        print(accession['sequence'][int(accession["features"][5]['begin'])-1:int(accession["features"][5]['end'])], file=f)
-        print(accession['sequence'][int(accession["features"][6]['begin'])-1:int(accession["features"][6]['end'])], file=f)
+        print(('>' + accession['accession']), file=f)
+        print((accession['sequence'][int(accession["features"][2]['begin'])-1:int(accession["features"][2]['end'])]+','+accession["features"][2]['begin']+','+accession["features"][2]['end']), file=f)
+        print((accession['sequence'][int(accession["features"][4]['begin'])-1:int(accession["features"][4]['end'])]+','+accession["features"][4]['begin']+','+accession["features"][4]['end']), file=f)
+        print((accession['sequence'][int(accession["features"][5]['begin'])-1:int(accession["features"][5]['end'])]+','+accession["features"][5]['begin']+','+accession["features"][5]['end']), file=f)
+        print((accession['sequence'][int(accession["features"][6]['begin'])-1:int(accession["features"][6]['end'])]+','+accession["features"][6]['begin']+','+accession["features"][6]['end']), file=f)
