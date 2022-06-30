099d405c06cae37908672ab52fda438d21f9096b
Sophia Leeman <sml96@cs.duke.edu>
2022-06-30 11:22:00 -0400

    Computed importances for all features

--- RF/TrainandTest.py
+++ RF/TrainandTest.py
@@ -1,12 +1,9 @@
 #This script tests the Random Forest on the entire preprocessed data set
 
 import CombineLigandsProteins
-import FeatureImportance
+import FixedClassificationModel
 
 CombineLigandsProteins.import_final()
 testX = CombineLigandsProteins.X
 testY = CombineLigandsProteins.Y
-AA_feat = CombineLigandsProteins.feat1
-Di_feat = CombineLigandsProteins.feat2
-Lig_feat = CombineLigandsProteins.feat3
-FeatureImportance.train(testX, testY, AA_feat, Di_feat, Lig_feat)
+FixedClassificationModel.train(testX, testY)
