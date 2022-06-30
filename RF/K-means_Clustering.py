76c609f29c44730050ddc5d832b08f5f5655468c
Sophia Leeman <sml96@cs.duke.edu>
2022-06-30 11:11:07 -0400

    Fixing k-means

--- RF/K-means_Clustering.py
+++ RF/K-means_Clustering.py
@@ -4,6 +4,7 @@
 import matplotlib.pyplot as plt
 from kneed import KneeLocator
 from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
 import numpy as np
 import CombineLigandsProteins
 from sklearn.model_selection import train_test_split
@@ -16,22 +17,28 @@ testY = CombineLigandsProteins.Y
 #split into training and test set
 X_train, X_test, y_train, y_test = train_test_split(testX, testY, stratify=testY, test_size=0.1) # 90% training and 10% test
 
-
-#Faster k-means (from: https://github.com/facebookresearch/faiss/wiki/Faiss-building-blocks:-clustering,-PCA,-quantization)
+#Cluster the data 
 import faiss
 import numpy as np
 
-#Cluster the data 
 d = X_train.shape[1]
 niter = 20
 verbose = True
 sse = [] #list holding SSE values
-for k in range(2,1000): #Test different number of clusters
+for k in range(2,2000,200): #Test different number of clusters
     ncentroids=k
     kmeans = faiss.Kmeans(d, ncentroids, niter = niter, verbose = verbose)
     kmeans.train(X_train.astype(np.float32))
     sse.append(kmeans.obj)
+"""
+sse = [] #list holding SSE values
+for k in range(2,2000,200): #Test different number of clusters
+    ncentroids=k
+    kmeans = KMeans(n_clusters=k, verbose=1)
+    kmeans.fit(X_train)
+    sse.append(kmeans.inertia_)
+"""
 
 #See how k decreases sse
-figure = plt.plot(range(2,1000), sse)
+figure = plt.plot(range(2,2000,200), sse)
 plt.savefig("ktest.png")
