fa7cc171c7efe59283b0379299ec94036ee07f41
Molly Borowiak <molly.borowiak@duke.edu>
2022-07-05 10:26:58 -0400

    testing to find runtime bottleneck

--- RF/TrainandTest.py
+++ RF/TrainandTest.py
@@ -2,24 +2,22 @@
 
 import CombineLigandsProteins
 import FixedClassificationModel
-#import FeatureImportance
 
 CombineLigandsProteins.import_final()
 testX = CombineLigandsProteins.X
 testY = CombineLigandsProteins.Y
 
-#FeatureImportance.train(testX, testY, CombineLigandsProteins.feats) 
-
-
 accuracy = 0
 recall = 0
 
-with open("scores.txt", "w") as f:
-    for i in range(10):
-        print("run " + str(i), file=f)
-        acc,rec = FixedClassificationModel.train(testX, testY)
-        accuracy += acc
-        recall += rec
+#acc,rec = FixedClassificationModel.train(testX, testY)
 
-    print('Average Accuracy: ' + str(accuracy/10), file=f)
-    print('Average Recall: ' + str(recall/10), file=f)
+
+for i in range(5):
+    print("run " + str(i))
+    acc,rec = FixedClassificationModel.train(testX, testY)
+    accuracy += acc
+    recall += rec
+
+print('Average Accuracy: ' + str(accuracy/5))
+print('Average Recall: ' + str(recall/5))
