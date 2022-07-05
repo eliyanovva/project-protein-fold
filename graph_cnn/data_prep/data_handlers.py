5b1be13891c51c46057351d7e8399170ffc1fed3
Teodora Petkova <teodorapetkovax@gmail.com>
2022-07-05 16:21:06 -0400

    separated data handlers class and ran model successfully

--- graph_cnn/data_prep/data_handlers.py
+++ graph_cnn/data_prep/data_handlers.py
@@ -1,133 +1,65 @@
+from abc import abstractmethod
 import numpy as np
 import pandas as pd
+import tensorflow as tf
 import logging as log
 import os
 import sys
 import re
+
 from . import log_config
 from . import constants
+from abc import ABC, abstractmethod
 
-class DataHandlers:
+class DataHandlers(ABC):
+    # THE CLASS IS MADE TO WORK ONLY WITH 2D MATRICES
     def __init__(self):
         pass
 
-    def createDatasetCSV(self):
-        with open('/home/users/tep18/new_ppp/project-protein-fold/graph_cnn/data_prep/uniprot_ligand_logfc_pvalue.csv', 'r') as couples_csv:
-            couples_list = couples_csv.readlines()
-            cur_dataset_list = couples_list
-            with open ('/home/users/tep18/new_ppp/project-protein-fold/graph_cnn/data_prep/dataset.csv', 'a') as dataset:
-                dataset.truncate(0)
-                #dataset.write('ProteinAdjacency, ProteinFeatures, LigandAdjacency, logFC\n')
-                for i in range(3):#len(cur_dataset_list)):
-                    line_list = cur_dataset_list[i].split(',')
-                    
-                    new_list = [0, 1, 2, 3]
 
-                    protein_data = self.fetchProteinData(line_list[0])
-                    ligand_data = self.fetchLigandData(line_list[1])
-                    new_list[0], new_list[1] = self.__convertNumpyToString(protein_data[0]), self.__convertNumpyToString(protein_data[1])
-                    log.info('first add successfull for row ' + str(i) + ' out of ' + str(len(cur_dataset_list)))
-                    new_list[2] = self.__convertNumpyToString(self.__fetchLigandData(line_list[1]))
-                    log.info('second add successfull for row ' + str(i) + ' out of ' + str(len(cur_dataset_list)))
-                    new_list[3] = (line_list[2])
-                    log.info('third add successfull for row ' + str(i) + ' out of ' + str(len(cur_dataset_list)))
+    def initialize(self, labels_list, dimensions, type):
+        # let dimensions be a tuple, not a list, so it can be passed as an argument
+        # to numpy and tensorflow functions.
+        self.labels_list = labels_list
+        self.dimensions = dimensions
+        self.type = type
+        log.info('Initialized data handler class')
+        
 
-                    
-                    #print(','.join(new_list))
-                    #dataset.write('\t'.join(new_list) + '\n')
+    def __fillMatrix(self):
+        log.info('Started filling out data matrix')
+        for i in range(len(self.labels_list)):
+            data = self.loadDataSingleMatrix(self.labels_list[i])
+            log.info('Succesfully loaded data for ' + self.labels_list[i])
+            self.matrix[i] = np.pad(
+                np.array(data, ndmin=2),
+                ((0, self.dimensions[0] - len(data)),
+                (0, self.dimensions[1] - len(data[0]))),
+                'constant',
+                constant_values=(0)
+            )
+            log.info('Padded and added data for ' + self.labels_list[i])
 
     
-    def createDatasetPickles(self):
-        with open('/home/users/tep18/new_ppp/project-protein-fold/graph_cnn/data_prep/uniprot_ligand_logfc_pvalue.csv', 'r') as couples_csv:
-            couples_list = couples_csv.readlines()
-            cur_dataset_list = couples_list
-            row_count = len(couples_list)
-            dataset_count = row_count // 250 + 1
-            print(dataset_count)
+    def getTensor(self):
+        log.info('Started Tensor extraction process')
+        self.__initializeNpMatrix()
+        self.__fillMatrix()
+        return tf.convert_to_tensor(self.matrix)
 
-            for i in range(dataset_count):
-                dataset_size = min(250, row_count - i * 250)
-                df = pd.DataFrame(index=range(dataset_size),columns=range(4))
-                
-                for j in range(i*250, i*250 + dataset_size):
-                    line_list = cur_dataset_list[j].split(',')
-                    protein_data = self.fetchProteinData(line_list[0])
-                    ligand_data = self.fetchLigandData(line_list[1])
-                    logFc = line_list[2]
+    @abstractmethod
+    def loadDataSingleMatrix(self, label_name):
+        raise NotImplementedError("Must override methodB")
 
-                    log.info('data extraction completed' + str(j))
-
-                    df.iat[j % 250, 0] = protein_data[0].resize((constants.PROTEIN_ADJACENCY_MAT_SIZE, constants.PROTEIN_ADJACENCY_MAT_SIZE))
-                    
-                    log.info('protein adjacency matrix resized and added to df')
-                    df.iat[j % 250, 1] = protein_data[1].resize((constants.PROTEIN_ADJACENCY_MAT_SIZE, constants.PROTEIN_FEATURES_COUNT))
-                    log.info('protein feature matrix resized and added to df')
-                    df.iat[j % 250, 2] = ligand_data.resize((constants.LIGAND_ADJACENCY_MAT_SIZE, constants.LIGAND_ADJACENCY_MAT_SIZE))
-                    log.info('ligand adjacency matrix resized and added to df')
-                    df.iat[j % 250, 3] = logFc
-                    log.info('dataframe load complete' +  str(j))
-
-                df.to_pickle(os.path.join(constants.MATRIX_DATA_FILES_PATH, 'dataset_part_' + str(i) + '.pkl'))
-
-
-    def concatenateDatasets(self):
-        data_prep_files = os.listdir('.')
-        for file in data_prep_files:
-            if file.endswith('.pkl'):
-                pass
-
-# TODO: fix the fact that this function exists in 2 other files :)
-    def fetchProteinData(self, protein_name):
-        # the protein name should be from the train/test X data.
-        protein_adjacency_matrix = np.load(
-            os.path.join(constants.PROTEIN_ADJACENCY_PATH, protein_name + '_adj_mat.npy')
-        )
-        protein_feature_matrix = np.load(
-            os.path.join(constants.PROTEIN_FEATURE_PATH, protein_name + '_feat_mat.npy')
-        )
-        return protein_adjacency_matrix, protein_feature_matrix
-
-
-    def fetchLigandData(self, ligand_name):
-        # the protein name should be from the train/test X data.
-        adjacency_file_name = self.__getLigandFileNames(ligand_name)
-        ligand_adjacency_matrix = np.load(
-            os.path.join(constants.MOL_ADJACENCY_PATH, adjacency_file_name)
-        )
-        return ligand_adjacency_matrix
     
-
-    def __convertNumpyToString(self, np_matrix):
-        log.info('started converting numpy file to string')
-        arr_str = "["
-        for row in np_matrix:
-            row_str = '['
-            for item in row:
-                row_str = row_str + str(item) + ","
-            row_str = row_str[:-1]
-            row_str = row_str + '],'
-            arr_str = arr_str + row_str
-        arr_str = arr_str[:-1]
-        arr_str = arr_str + "]"
-        log.info('finished numpy file conversion')
-        #print(arr_str)
-        return arr_str
-
-
-    def __getLigandFileNames(self, ligand_name):
-        """The function returns the filename of the ligand adjacency matrix corresponding to the
-        ligand name from the ligand column in the uniprot-ligand-logFC-pValue csv.
-
-        Args:
-            ligand_name (_type_): _description_
-        """
-
-        mylist = os.listdir(constants.MOL_ADJACENCY_PATH)
-        left_index = ligand_name.rfind('_')
-        r = re.compile(".*"+ ligand_name[left_index:] + "_adj_mat.npy")
-        adjacency_matrix_filename = list(filter(r.match, mylist))
-        return adjacency_matrix_filename[0]
-
-
-#dh = DataHandlers()
-#dh.createDatasetPickles()
+    def __initializeNpMatrix(self):
+        self.matrix = np.zeros(
+            (
+                len(self.labels_list),
+                self.dimensions[0],
+                self.dimensions[1]
+            ),
+            dtype = self.type
+        )
+        log.info('Completed zero matrix initialization')
+    
--- graph_cnn/model.py
+++ graph_cnn/model.py
@@ -1,65 +1,38 @@
+from stringprep import in_table_c8
 import tensorflow as tf
 import numpy as np
 import pandas as pd
 import logging as log
-from sklearn.model_selection import train_test_split
-from data_prep.data_handlers import DataHandlers
 import os
+from sklearn.model_selection import train_test_split
 
 import data_prep.log_config
+import data_prep.constants as constants
 from protein_handlers import ProteinAdjacencyData, ProteinFeatureData
 from ligand_handlers import LigandAdjacencyData
-import data_prep.constants as constants
-
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
-
-config = ConfigProto()
-config.gpu_options.per_process_gpu_memory_fraction = 0.4
-session = InteractiveSession(config=config)
+from data_prep.data_handlers import DataHandlers
 
 
 log.info(os.getpid())
 
 class GraphCNN:
     def __init__(self):
+        pass
+
+    
+    def initialize(self):
         self.prot_adj_data_handler = ProteinAdjacencyData()
         self.prot_feat_data_handler = ProteinFeatureData()
         self.lig_adj_data_handler = LigandAdjacencyData()
-        
         log.info('initialized GraphCNN class')
-        self.dataset = pd.read_pickle(os.path.join(constants.MATRIX_DATA_FILES_PATH, 'dataset_part_0.pkl'))
-        log.info('Loaded dataset')
-        self.y = self.dataset.iloc[:,3]
-        log.info('Loaded target columns')
-        self.dataset = self.dataset.drop(self.dataset.columns[[3]],axis = 1)
-        log.info('Removed y column from dataset')
-        
-        
-#        self.dataset = tf.data.experimental.make_csv_dataset(
-#            './dataset.csv',
-#            column_names=['ProteinAdjacency', 'ProteinFeatures', 'LigandAdjacency', 'logFC'],
-#            batch_size=4, # Artificially small to make examples easier to show.
-#            label_name='logFC',
-#            num_epochs=1,
-#            header=True
-            #ignore_errors=True
-#            )
-        #chunksize = 10 ** 6
-        #textFileReader = pd.read_csv('dataset.csv', chunksize=chunksize)
-        #full_data = pd.concat(textFileReader, ignore_index=True)
-        #self.dataset = pd.read_csv('dataset.csv')
-        #print('finished reading csv')
-        #self.y = self.dataset.iloc[:, 2]
-        #self.dataset.drop([2], axis=1)
-    
-    
+
+
     def trainTestSplit(self):
         with open(constants.MATRIX_DATA_FILES_PATH + '/uniprot_ligand_logfc_pvalue.csv', 'r') as comb_file:
             comb_file_lines = self.__getValidEntries(comb_file.readlines())
             x, y = [], []
             # FIX SIZE LATER, SET TO 250 FOR A SMALLER BATCH TRY
-        for row in comb_file_lines[:100]:
+        for row in comb_file_lines[300]:
             row = row[:-1].split(',')
             if len(row) == 4:
                 protein_ligand_list = []
@@ -72,15 +45,13 @@ class GraphCNN:
         X_train, X_test, y_train, y_test = train_test_split(
             x, y, test_size=0.3
             )
-        #log.info('Split data in training and testing sets.')
+
+        log.info('Split data in training and testing sets.')
+
         return X_train, X_test, y_train, y_test
 
 
     def createModel(self):
-        #undid '#' infront of these three lines
-        prot_adj_model = self.prot_adj_data_handler.createModel()
-        prot_feat_model = self.prot_feat_data_handler.createModel()
-        lig_adj_model = self.lig_adj_data_handler.createModel()
         
         prot_adj_in = tf.keras.layers.Input(
             shape=(constants.PROTEIN_ADJACENCY_MAT_SIZE,
@@ -100,7 +71,6 @@ class GraphCNN:
             name='Ligand-Adjacency-Matrix'
         )
  
-        # the first branch operates on the first input
         x = tf.keras.layers.Conv1D(
             2, 3, activation='relu'
         )(prot_adj_in)
@@ -108,48 +78,32 @@ class GraphCNN:
         x = tf.keras.layers.Flatten()(x)
         x = tf.keras.layers.Dense(128, activation="relu")(x)
         x = tf.keras.Model(inputs=prot_adj_in, outputs=x)
-        # the second branch opreates on the second input
         
         y = tf.keras.layers.Flatten()(prot_feat_in)
         y = tf.keras.layers.Dense(32, activation="relu")(y)
-        #y = tf.keras.layers.Dense(4, activation="relu")(y)
+        y = tf.keras.layers.Dense(4, activation="relu")(y)
         y = tf.keras.Model(inputs=prot_feat_in, outputs=y)
 
         z = tf.keras.layers.Flatten()(ligand_adj_in)
         z = tf.keras.layers.Dense(32, activation="relu")(z)
-        #z = tf.keras.layers.Dense(4, activation="relu")(z)
+        z = tf.keras.layers.Dense(4, activation="relu")(z)
         z = tf.keras.Model(inputs=ligand_adj_in, outputs=z)
-        # combine the output of the two branches
+        
         combined = tf.keras.layers.concatenate([x.output, y.output, z.output])
-        # apply a FC layer and then a regression prediction on the
-        # combined outputs
-        out = tf.keras.layers.Dense(2, activation="relu")(combined)
+        
+
+        out = tf.keras.layers.Dense(128, activation="relu")(combined)
         out = tf.keras.layers.Dense(1, activation="linear")(out)
-        # our model will accept the inputs of the two branches and
-        # then output a single value
+        
         model = tf.keras.Model(inputs=[x.input, y.input, z.input], outputs=out)
 
         print(model.summary())
-        #concat_layer = tf.keras.layers.Concatenate([
-        #    prot_adj_model.output,
-        #    prot_feat_model.output,
-        #    lig_adj_model.output
-       # ])
 
-        #output = tf.keras.layers.Dense(1024, activation='relu')(concat_layer)
-        #output = tf.keras.layers.Dense(512, activation='relu')(output)
-        #output = tf.keras.layers.Dense(128, activation='relu')(output)
-        #output = tf.keras.layers.Dense(1, activation='relu')(output)    
-        
-        #model = tf.keras.models.Model([
-        #    prot_adj_model.input,
-        #    prot_feat_model.input,
-        #    lig_adj_model.input
-        #    ], 
-        #    outputs=output
-        #)
-
-        model.compile(optimizer='adam', loss='mean_squared_error', metrics=['accuracy'])
+        model.compile(
+            optimizer='adam',
+            loss=tf.keras.losses.MeanSquaredLogarithmicError(),
+            metrics=[tf.keras.metrics.RootMeanSquaredError(), tf.keras.metrics.Accuracy()]
+        )
         return model
 
 
@@ -179,136 +133,72 @@ class GraphCNN:
         return comb_file_lines
 
 
-    def __fetchProteinData(self, protein_name):
-        # the protein name should be from the train/test X data.
-        protein_adjacency_matrix = np.load(os.path.join(constants.PROTEIN_ADJACENCY_PATH, protein_name + '_adj_mat.npy'))
-        protein_feature_matrix = np.load(os.path.join(constants.PROTEIN_FEATURE_PATH, protein_name + '_feat_mat.npy'))
-        return protein_adjacency_matrix, protein_feature_matrix
+    def getTensors(self, X, y):
+        X_proteins = [row[0] for row in X]
+        X_ligands = [row[1] for row in X]
+        self.prot_adj_data_handler.initialize(
+            X_proteins, 
+            (constants.PROTEIN_ADJACENCY_MAT_SIZE, constants.PROTEIN_ADJACENCY_MAT_SIZE),
+            np.int8
+        )
+        tf_prot_adjacency = self.prot_adj_data_handler.getTensor()
 
+        self.prot_feat_data_handler.initialize(
+            X_proteins, 
+            (constants.PROTEIN_ADJACENCY_MAT_SIZE, constants.PROTEIN_FEATURES_COUNT),
+            float
+        )
+        tf_prot_features = self.prot_feat_data_handler.getTensor()
 
-    def __fetchLigandData(self, ligand_file_name):
-        ligand_file_names = os.listdir(constants.MOL_ADJACENCY_PATH)
-        data_file_name = self.__getLigandName(ligand_file_names, ligand_file_name)
-        ligand_adjacency_matrix = np.load(os.path.join(constants.MOL_ADJACENCY_PATH, data_file_name))
-        return ligand_adjacency_matrix
-
-
-    def __getLigandName(self, files_list, ligand_name):
-        res = None
-        for name in files_list:
-            if ligand_name in name:
-                res = name
-                
-        return res
+        self.lig_adj_data_handler.initialize(
+            X_ligands, 
+            (constants.LIGAND_ADJACENCY_MAT_SIZE, constants.LIGAND_ADJACENCY_MAT_SIZE),
+            np.int8
+        )
+        tf_lig_adjacency = self.lig_adj_data_handler.getTensor()
         
+        tf_logfc = tf.strings.to_number(
+            tf.convert_to_tensor(np.array(y)),
+            out_type=tf.dtypes.float32
+        )
+        
+        return [tf_prot_adjacency, tf_prot_features, tf_lig_adjacency], tf_logfc
+
+
+
+
 g = GraphCNN()
-X_train, X_test, y_train, y_test = g.trainTestSplit()
+g.initialize()
+X_train_labels, X_test_labels, y_train_labels, y_test_labels = g.trainTestSplit()
 
-np_prot_adjacency = np.zeros((
-    len(X_train),
-    constants.PROTEIN_ADJACENCY_MAT_SIZE,
-    constants.PROTEIN_ADJACENCY_MAT_SIZE
-    ), dtype = 'int8'
-)
+X_train, y_train = g.getTensors(X_train_labels, y_train_labels)
+X_test, y_test = g.getTensors(X_test_labels, y_test_labels)
 
-np_prot_features = np.zeros((
-    len(X_train),
-    constants.PROTEIN_ADJACENCY_MAT_SIZE,
-    constants.PROTEIN_FEATURES_COUNT)
-)
-
-np_ligand_adjacency = np.zeros((
-    len(X_train),
-    constants.LIGAND_ADJACENCY_MAT_SIZE,
-    constants.LIGAND_ADJACENCY_MAT_SIZE), dtype='int8'
-)
-
-np_logfc = np.array(y_train)
-
-log.info('Began initializing the arrays')
-for i in range(len(X_train)):
-    dh = DataHandlers()
-
-    log.info('NP arrays start loading')
-    protein_data = dh.fetchProteinData(X_train[i][0])
-    ligand_data = dh.fetchLigandData(X_train[i][1])
-    log.info('np arrays loading finished')
-
-    protein_adjacency_matrix = np.pad(
-        np.array(protein_data[0], ndmin=2),
-        ((0, constants.PROTEIN_ADJACENCY_MAT_SIZE - len(protein_data[0])),
-        (0, constants.PROTEIN_ADJACENCY_MAT_SIZE - len(protein_data[0][0]))),
-        'constant',
-        constant_values=(0)
-    )
-
-    protein_feature_matrix = np.pad(
-        np.array(protein_data[1], ndmin=2),
-        ((0, constants.PROTEIN_ADJACENCY_MAT_SIZE - len(protein_data[1])),
-        (0, constants.PROTEIN_FEATURES_COUNT - len(protein_data[1][0]))),
-        'constant',
-        constant_values=(0)
-    )
-
-    ligand_adjacency_matrix = np.pad(
-        np.array(ligand_data, ndmin=2),
-        ((0, constants.LIGAND_ADJACENCY_MAT_SIZE - len(ligand_data)),
-        (0, constants.LIGAND_ADJACENCY_MAT_SIZE - len(ligand_data[0]))),
-        'constant',
-        constant_values=(0)
-    )
-        
-    np_prot_adjacency[i] = protein_adjacency_matrix
-    np_prot_features[i] = protein_feature_matrix
-    np_ligand_adjacency[i] = ligand_adjacency_matrix
-
-    log.info('Done with initializing the arrays ')
-
-log.info('start converting numpy data to tensors')
-tf_prot_adjacency = tf.convert_to_tensor(
-    np_prot_adjacency
-    #shape=[len(X_train), constants.PROTEIN_ADJACENCY_MAT_SIZE, constants.PROTEIN_ADJACENCY_MAT_SIZE],
-    #dtype=int
-)
-tf_prot_features = tf.convert_to_tensor(
-    np_prot_features
-    #shape=(len(X_train), constants.PROTEIN_ADJACENCY_MAT_SIZE, constants.PROTEIN_FEATURES_COUNT),
-    #dtype=float
-)
-tf_ligand_adjacency = tf.convert_to_tensor(
-    np_ligand_adjacency
-#    shape=(len(X_train), constants.LIGAND_ADJACENCY_MAT_SIZE, constants.LIGAND_ADJACENCY_MAT_SIZE),
-#    dtype=int
-)
-print(y_train[0])
-
-tf_logfc = tf.strings.to_number(tf.convert_to_tensor(np_logfc), out_type=tf.dtypes.float32)
-
-print(tf_ligand_adjacency.dtype, tf_logfc.dtype, tf_prot_adjacency.dtype, tf_prot_features.dtype)
-
-log.info('completed tensor generation')
 model = g.createModel()
-#print(X_train)
-#model = g.createModel()
 
 log.info('model fitting started')
 
-model.fit([tf_prot_adjacency, tf_prot_features, tf_ligand_adjacency],
-          tf_logfc,
-          epochs=10, verbose=True, batch_size=1)
-
+mod_history = model.fit(X_train, y_train, epochs=10, verbose=True, batch_size=1)
 log.info ('model fitting finished successfully')
 
-#log.info('model evaluation started')
-#print(model.evaluate([X_test.iloc[:,0], X_test.iloc[:,1], X_test.iloc[:,2]],
-#          y_test, verbose=False))
-#log.info('model evaluation completed')"""
-#print(len(a))
-#print(g.fetchProteinData(a[4000][0]))
+log.info('model evaluation started')
+print(model.evaluate(X_test, y_test, verbose=True))
+log.info('model evaluation completed')
 
-#print(b)
-#print(d)
-#with open('testf.txt', 'a') as test_file:
-##    test_file.write(','.join(','.join(b)))
-  #  test_file.write(','.join(d))
+
+#import matplotlib.pyplot as plt
+
+# Plot the results
+#acc = mod_history.history['accuracy']
+#loss = mod_history.history['loss']
+
+#epochs = range(len(acc))
+
+#plt.plot(epochs, acc, 'r', label='Training accuracy')
+#plt.plot(epochs, val_acc, 'b', label='Validation accuracy')
+#plt.title('Training accuracy')
+#plt.legend(loc=0)
+#plt.figure()
+
+#plt.savefig('training_accuracy.png')
 
