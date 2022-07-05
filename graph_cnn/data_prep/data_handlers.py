6468f7c992749504f3be03959bb6f678cfd144d4
Teodora Petkova <teodorapetkovax@gmail.com>
2022-07-05 16:50:29 -0400

    Added docstrings

--- graph_cnn/data_prep/data_handlers.py
+++ graph_cnn/data_prep/data_handlers.py
@@ -1,30 +1,68 @@
-from abc import abstractmethod
-import numpy as np
-import pandas as pd
-import tensorflow as tf
+"""The DataHandlers class helps generate numpy arrays which are fed into the GNN model.
+It uses the .npy adjacency and feature matrices which are already generated. Its unique
+features are that it makes them the same size, and generated a big numpy array for each
+type of matrix, which can hold all training protein adjacency data, ot all training feature data
+and so on.
+"""
 import logging as log
-import os
-import sys
-import re
-
-from . import log_config
-from . import constants
 from abc import ABC, abstractmethod
 
+import numpy as np
+import tensorflow as tf
+
+from . import log_config
+
+
 class DataHandlers(ABC):
-    # THE CLASS IS MADE TO WORK ONLY WITH 2D MATRICES
+    """This is an abstract class used to generate Tensors from .npy matrices.
+    It is made to work only with 2D matrices.
+
+    Raises:
+        NotImplementedError: This error is raised if the method used to extract
+        specific matrix type from a .npy file is not defined.
+    """
     def __init__(self):
         pass
 
 
-    def initialize(self, labels_list, dimensions, type):
-        # let dimensions be a tuple, not a list, so it can be passed as an argument
-        # to numpy and tensorflow functions.
+    def initialize(self, labels_list, dimensions, data_type):
+        """Initialization method for the DataHandlers class.
+
+        Args:
+            labels_list (List[str]): A list of strings which are protein or ligand names.
+            dimensions (tuple): A tuple (x, y) with the dimensions of the matrix type to be handled
+            type (np.type): the datatype of the matrix - int or float.
+        """
         self.labels_list = labels_list
         self.dimensions = dimensions
-        self.type = type
+        self.type = data_type
         log.info('Initialized data handler class')
-        
+
+
+    def getTensor(self):
+        """Returns a tensor containing all requested matrices.
+
+        Returns:
+            tf.Tensor: A Tensor which concatenates all matrices labeled in the labels_list.
+        """
+        log.info('Started Tensor extraction process')
+        self.__initializeNpMatrix()
+        self.__fillMatrix()
+        return tf.convert_to_tensor(self.matrix)
+
+    @abstractmethod
+    def loadDataSingleMatrix(self, label_name):
+        """This method loads a single .npy array to a numpy array and returns it. It has
+        to be defined be every child class.
+
+        Args:
+            label_name (string): The name of the protein/ligand whose data we need.
+
+        Raises:
+            NotImplementedError: Raised if the method is not implemented in the child class.
+        """
+        raise NotImplementedError("Must override methodB")
+
 
     def __fillMatrix(self):
         log.info('Started filling out data matrix')
@@ -40,18 +78,7 @@ class DataHandlers(ABC):
             )
             log.info('Padded and added data for ' + self.labels_list[i])
 
-    
-    def getTensor(self):
-        log.info('Started Tensor extraction process')
-        self.__initializeNpMatrix()
-        self.__fillMatrix()
-        return tf.convert_to_tensor(self.matrix)
 
-    @abstractmethod
-    def loadDataSingleMatrix(self, label_name):
-        raise NotImplementedError("Must override methodB")
-
-    
     def __initializeNpMatrix(self):
         self.matrix = np.zeros(
             (
@@ -62,4 +89,3 @@ class DataHandlers(ABC):
             dtype = self.type
         )
         log.info('Completed zero matrix initialization')
-    
