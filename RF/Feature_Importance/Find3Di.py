63a3884234ed69cca4555bafbb990b5312a9e54d
Sophia Leeman <sml96@cs.duke.edu>
2022-07-06 13:02:23 -0400

    Made 3Di feature visualization script

--- RF/Feature_Importance/Find3Di.py
+++ RF/Feature_Importance/Find3Di.py
@@ -27,10 +27,6 @@ def resinumber(protein, threeDi, domain): #domain is 1, 2, 3, or 4 corresponding
         ret.append(start + entry)
     return ret
 
-print(resinumber('Q8VET4', 'LQ', 1))
-
-
-"""
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     if os.path.isfile(f):
@@ -38,5 +34,74 @@ for filename in os.listdir(directory):
         cmd.load(location_of_protein) #Load in pdb file
         name_of_protein = filename.replace("-model_v2.pdb", "")
         cmd.color("magenta", name_of_protein)
+        
+        name = name_of_protein.replace("AF-", "")
+        name = name.replace("-F1", "")
+        #Visualize the 3Di features
+        printstatement1 = ""
+        i = 0
+        for structure in resinumber(name,'QVVCV', 3):
+            if i > 0:
+                printstatement1 += ' or '
+            printstatement1 += 'Resi ' + str(structure) + '- ' + str(structure+5)
+            i = 1
+        print(printstatement1)
 
-    """
+        printstatement2 = ""
+        i = 0
+        for structure in resinumber(name,'PPNVS', 4):
+            if i > 0:
+                printstatement2 += ' or '
+            printstatement2 += 'Resi ' + str(structure) + '- ' + str(structure+5)
+            i = 1
+        print(printstatement2)
+
+        printstatement3 = ""
+        i = 0
+        for structure in resinumber(name,'PNPSS', 4):
+            if i > 0:
+                printstatement3 += ' or '
+            printstatement3 += 'Resi ' + str(structure) + '- ' + str(structure+5)
+            i = 1
+        print(printstatement3)
+
+        printstatement4 = ""
+        i = 0
+        for structure in resinumber(name,'VNPLV', 3):
+            if i > 0:
+                printstatement4 += ' or '
+            printstatement4 += 'Resi ' + str(structure) + '- ' + str(structure+5)
+            i = 1
+        print(printstatement4)
+
+        printstatement5 = ""
+        i = 0
+        for structure in resinumber(name,'VVCCV', 4):
+            if i > 0:
+                printstatement5 += ' or '
+            printstatement5 += 'Resi ' + str(structure) + '- ' + str(structure+5)
+            i = 1
+        print(printstatement5)
+
+        #Color important structures
+        cmd.select("Di1", printstatement1)
+        cmd.color("red", "Di1")
+
+        cmd.select("Di2", printstatement2)
+        cmd.color("orange", "Di2")
+
+        cmd.select("Di3", printstatement3)
+        cmd.color("yellow", "Di3")
+
+        cmd.select("Di4", printstatement4)
+        cmd.color("green", "Di4")
+
+        cmd.select("Di5", printstatement5)
+        cmd.color("blue", "Di5")
+
+        #Save as png
+        savelocation = "Images/" + "Di_" + name_of_protein + ".png"
+        cmd.png(savelocation)
+        
+        #Hide protein
+        cmd.hide("everything", name_of_protein)
