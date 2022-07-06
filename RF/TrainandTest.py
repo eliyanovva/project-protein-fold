b579121dff4e1854976d2b0cd0b46dbdb2525c6d
Sophia Leeman <sml96@cs.duke.edu>
2022-07-06 10:23:22 -0400

    The accuracy is better with cv=5 and not much slower

--- RF/TrainandTest.py
+++ RF/TrainandTest.py
@@ -1,17 +1,17 @@
 #This script tests the Random Forest on the entire preprocessed data set
 
 import CombineLigandsProteins
-#import FixedClassificationModel
-import RF.Feature_Importance.FeatureImportance as FeatureImportance
+import FixedClassificationModel
+#import RF.Feature_Importance.FeatureImportance as FeatureImportance
 
 CombineLigandsProteins.import_final()
 testX = CombineLigandsProteins.X
 testY = CombineLigandsProteins.Y
 
-FeatureImportance.train(testX, testY, CombineLigandsProteins.feats) 
+#FeatureImportance.train(testX, testY, CombineLigandsProteins.feats) 
 
 
-#FixedClassificationModel.train(testX, testY)
+FixedClassificationModel.train(testX, testY)
 
 """
 accuracy = 0
