b8d3bd71bc76bf585ed3a9e8487b0dddfee78b50
Molly Borowiak <molly.borowiak@duke.edu>
2022-07-08 15:00:05 -0400

    include balanced accuracy score in reported results

--- RF/FixedClassificationModel.py
+++ RF/FixedClassificationModel.py
@@ -36,7 +36,8 @@ def train(features, labels):
     print("Recall:",rec)
 
     y_pred=clf.predict(X_test)
-    print(metrics.balanced_accuracy_score(y_test, y_pred))
+    bac = metrics.balanced_accuracy_score(y_test, y_pred)
+    print('Balanced Accuracy Score: ' + str(bac))
 
-    return acc,rec
+    return acc,rec,bac
 
--- RF/TrainandTest.py
+++ RF/TrainandTest.py
@@ -9,19 +9,24 @@ CombineLigandsProteins.import_final()
 testX = CombineLigandsProteins.X
 testY = CombineLigandsProteins.Y
 
-AdjustingThreshold.train(testX, testY)
+#AdjustingThreshold.train(testX, testY)
 
 #FeatureImportance.train(testX, testY, CombineLigandsProteins.feats)
-FixedClassificationModel.train(testX, testY)
+#FixedClassificationModel.train(testX, testY)
 
-"""
+
 accuracy = 0
 recall = 0
+BAC = 0
+f = open('results.txt', 'w')
 for i in range(50):
     print("run " + str(i))
-    acc,rec = FixedClassificationModel.train(testX, testY)
+    acc,rec,bac = FixedClassificationModel.train(testX, testY)
     accuracy += acc
     recall += rec
+    BAC += bac
+    f.write(str(acc)+ ", " + str(rec) + ", " + str(bac) + "\n")
 print('Average Accuracy: ' + str(accuracy/50))
 print('Average Recall: ' + str(recall/50))
-"""
+print('Average Balanced: ' + str(BAC/50))
+f.close()
