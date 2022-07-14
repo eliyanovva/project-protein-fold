4d471bc7188cdbfe6f99b33bb007277945d8b84c
Sophia Leeman <sml96@cs.duke.edu>
2022-07-14 09:50:10 -0400

    Updated pictures and sulfur importance

--- RF/Feature_Importance/FeatureImportance.py
+++ RF/Feature_Importance/FeatureImportance.py
@@ -31,7 +31,7 @@ def train(features, labels):
 def importance_file(features, labels, filter_feat):
     #Writes the features of the model in order of importance to the important_features.txt file
     importances = train(features, labels)
-    for i in range(0,1000,1):
+    for i in range(0,10000,1):
         importances += train(features,labels)
     with open("Feature_Importance/sulfur_importance.txt", "w") as f:
         while np.argmax(importances) > 0:
