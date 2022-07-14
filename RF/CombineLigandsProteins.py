deb91033f5ccadda9a50584c52a17672bf668b17
Molly Borowiak <molly.borowiak@duke.edu>
2022-07-14 15:18:04 -0400

    revised comments for CombineLigandsProteins.py; employed filtered lig kmers in final matrix

--- RF/CombineLigandsProteins.py
+++ RF/CombineLigandsProteins.py
@@ -13,29 +13,37 @@ import Duplicates
 acc_ids = Globals.initialize_protein_list()
 logFC, FDR = labels.labels()
 classified, pos_counts, neg_counts, pos_pairs, neg_pairs = labels.classified_logFC_FDR(logFC, FDR, acc_ids)
+#classified = key: protein id, value: (key = ligand, value = {1 if bind, 0 if not bind})
+#pos_counts = key: protein id, value: number of positive protein interactions
+#neg_counts = key: protein id, value: number of negative protein interactions
+#pos_pairs = list of positive protein-ligand pairs; pos_pairs[i] = [protein id, ligand]
+#neg_pairs = list of negative protein-ligand pairs; neg_pairs[i] = [protein id, ligand]
 
-proteins_toconsider = set()     #has 392 proteins
-ligands_toconsider = set()      #has 49 ligands
+proteins_toconsider = set()     #proteins that can form either a positive or negative pair with a ligand
+ligands_toconsider = set()      #ligands that can form either a positive or negative pair with a protein
 
+#Extract the proteins and ligands that interact in positive or negative pairs;
+#Only these proteins and ligands can be used in the feature matrix
 for pair in pos_pairs:
     proteins_toconsider.add(pair[0])
     ligands_toconsider.add(pair[1])
-
 for pair in neg_pairs:
     proteins_toconsider.add(pair[0])
     ligands_toconsider.add(pair[1])
 
-pairs_by_prot = {}
+pairs_by_prot = {}      #key = protein id, value = # of pairs involving the protein
 
-for id in proteins_toconsider:
-    pairs_by_prot[id] = 0
-
+#Iterate through all positive and negative prot-lig pairs to update pairs_by_prot
 for pair in pos_pairs:
     id = pair[0]
+    if id not in pairs_by_prot:
+        pairs_by_prot[id] = 0
     pairs_by_prot[id] += 1
 
 for pair in neg_pairs:
     id = pair[0]
+    if id not in pairs_by_prot:
+        pairs_by_prot[id] = 0
     pairs_by_prot[id] += 1
 
 #Initialize Variables
@@ -66,26 +74,29 @@ di_features_TM7 = set()
 di_seqs_TM7 = {}
 di_matrix_TM7 = []
 
-
+#Create dict of AA sequences only with proteins from pos or neg pairs
+AA_dict = Globals.initialize_AA_dict(list(proteins_toconsider))
 #Create AA output for TMs 3,5,6,7
-AA_dict = Globals.initialize_AA_dict(list(proteins_toconsider))   #create dict with proteins from pos / neg pairs
 AA_seqvar_TM3, AA_features_TM3 = ReadingFasta.make_seqvar_TMS(AA_dict, 0, 5, categorized_seqs_TM3, categorized_features_TM3)
 AA_seqvar_TM5, AA_features_TM5 = ReadingFasta.make_seqvar_TMS(AA_dict, 1, 5, categorized_seqs_TM5, categorized_features_TM5)
 AA_seqvar_TM6, AA_features_TM6 = ReadingFasta.make_seqvar_TMS(AA_dict, 2, 5, categorized_seqs_TM6, categorized_features_TM6)
 AA_seqvar_TM7, AA_features_TM7 = ReadingFasta.make_seqvar_TMS(AA_dict, 3, 5, categorized_seqs_TM7, categorized_features_TM7)
 
+#Filter the AA kmers for TMs 3,5,6,7
 AA_filter_TM3, feat1 = Filtering.richness_protein(AA_features_TM3, AA_seqvar_TM3, pos_counts, neg_counts, "TM3")
 AA_filter_TM5, feat2 = Filtering.richness_protein(AA_features_TM5, AA_seqvar_TM5, pos_counts, neg_counts, "TM5")
 AA_filter_TM6, feat3 = Filtering.richness_protein(AA_features_TM6, AA_seqvar_TM6, pos_counts, neg_counts, "TM6")
 AA_filter_TM7, feat4 = Filtering.richness_protein(AA_features_TM7, AA_seqvar_TM7, pos_counts, neg_counts, "TM7")
 
-#Create 3Di output for Tms 3,5,6,7
+#Create dict of 3Di sequences only with proteins from pos or neg pairs
 Di_dict = Globals.initialize_3Di_dict(list(proteins_toconsider))
+#Create 3Di output for TMs 3,5,6,7
 Di_seqvar_TM3, Di_features_TM3 = ReadingFasta.make_seqvar_TMS(Di_dict, 0, 5, di_seqs_TM3, di_features_TM3)
 Di_seqvar_TM5, Di_features_TM5 = ReadingFasta.make_seqvar_TMS(Di_dict, 1, 5, di_seqs_TM5, di_features_TM5)
 Di_seqvar_TM6, Di_features_TM6 = ReadingFasta.make_seqvar_TMS(Di_dict, 2, 5, di_seqs_TM6, di_features_TM6)
 Di_seqvar_TM7, Di_features_TM7 = ReadingFasta.make_seqvar_TMS(Di_dict, 3, 5, di_seqs_TM7, di_features_TM7)
 
+#Filter the 3Di kmers for TMs 3,5,6,7
 Di_filter_TM3, feat5 = Filtering.richness_protein(Di_features_TM3, Di_seqvar_TM3, pos_counts, neg_counts, "TM3")
 Di_filter_TM5, feat6 = Filtering.richness_protein(Di_features_TM5, Di_seqvar_TM5, pos_counts, neg_counts, "TM5")
 Di_filter_TM6, feat7 = Filtering.richness_protein(Di_features_TM6, Di_seqvar_TM6, pos_counts, neg_counts, "TM6")
@@ -96,37 +107,43 @@ AA_feat = [AA_filter_TM3, AA_filter_TM5, AA_filter_TM6, AA_filter_TM7]
 Di_seqvar = [Di_seqvar_TM3, Di_seqvar_TM5, Di_seqvar_TM6, Di_seqvar_TM7]
 Di_feat = [Di_filter_TM3, Di_filter_TM5, Di_filter_TM6, Di_filter_TM7]
 
-unique_proteins = Duplicates.remove_proteins(AA_seqvar, AA_feat, Di_seqvar, Di_feat, pairs_by_prot, proteins_toconsider)
+#Extract proteins with unique AA and 3di kmer frequencies
+unique_proteins = Duplicates.remove_proteins(AA_seqvar, AA_feat, Di_seqvar, Di_feat, pairs_by_prot, list(proteins_toconsider))
 
-pos_dict = {}
-neg_dict = {}
+pos_dict = {}       #key = protein id, value = list of ligands that the protein binds with
+neg_dict = {}       #key = protein id, value = list of ligands that the protein does not bind with
 
 for id in unique_proteins:
     pos_dict[id] = []
     neg_dict[id] = []
 
+#Iterate through all positive prot-lig pairs to update pos_dict
 for pair in pos_pairs:
     id = pair[0]
     lig = pair[1]
-    if (id in pos_dict) & (lig in ligands_toconsider):
+    #Check that id is from the set of unique proteins
+    if (id in pos_dict):
         pos_dict[id].append(lig)
-
+#Iterate through all negative prot-lig pairs to update neg_dict
 for pair in neg_pairs:
     id = pair[0]
     lig = pair[1]
-    if (id in neg_dict) & (lig in ligands_toconsider):
+    #Check that id is from the set of unique proteins
+    if (id in neg_dict):
         neg_dict[id].append(lig)
 
 #Import dictionary matching ligands to SMILES String
 ligand_dict = Globals.initialize_ligand_dict()
+
 #Create ligands matrix
 ligand_features, ligand_counts = SmileKmer.ligand_matrix(ligand_dict, 5, ligands_toconsider)
+#ligand_features = list of kmers found in the ligands with pos / neg pairs
+#ligand_counts = key: ligand, value: dict (key: kmer, value: freq. of kmer in the ligand)
+#   ligand_counts only uses ligands from ligands_toconsider as keys
 
-#ligand_counts only has ligands from ligands_toconsider as keys
-
-pos_by_lig = {}
-neg_by_lig = {}
-total_by_lig = {}
+pos_by_lig = {}         #key: ligand, value: # of pos. pairs with the ligand
+neg_by_lig = {}         #key: ligand, value: # of neg. pairs with the ligand
+total_by_lig = {}       #key: ligand, value: # of all pairs the involve the ligand
 
 for lig in ligands_toconsider:
     pos_by_lig[lig] = 0
@@ -144,16 +161,18 @@ for id in unique_proteins:
         neg_by_lig[lig] += 1
         total_by_lig[lig] += 1
 
+#Update ligand_counts to only use filtered kmers
 lig_counts_filter = Filtering.richness_ligand(ligand_counts, pos_by_lig, neg_by_lig)
+#Extract ligands with unique kmer frequencies
 unique_ligands = Duplicates.remove_ligands(ligand_counts, total_by_lig)
 
-pos_total = 0
-neg_total = 0
-lig_mat = []
+pos_total = 0           #num. of positive pairs with ligands from unique_ligands
+neg_total = 0           #num. of negative pairs with ligands from unique_ligands
+lig_mat = []            #matrix to store ligand features
 for id in unique_proteins:
     for lig in pos_dict[id]:
         if (unique_ligands.count(lig) != 0):
-            lig_mat.append(np.array(list(ligand_counts[lig].values())))
+            lig_mat.append(np.array(list(lig_counts_filter[lig].values())))
             pos_total += 1
 
 for id in unique_proteins:
