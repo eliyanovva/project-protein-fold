559df8d20cf835f2e1fc7c02f90d9b4ae16b5e41
Sophia Leeman <sml96@cs.duke.edu>
2022-07-15 09:51:56 -0400

    Need to fix the case where there is no unique mode

--- RF/Feature_Importance/Location_of_importances.py
+++ RF/Feature_Importance/Location_of_importances.py
@@ -5,29 +5,31 @@ from statistics import mode
 
 AA_seqs = Globals.initialize_AA_dict(Globals.initialize_protein_list())
 Di_seqs = Globals.initialize_3Di_dict(Globals.initialize_protein_list())
+tmdict = {'3':0, '5':1, '6':2, '7':3}
 
-def findAA(tm, seq):
+def find_feature(tm, seq, dictionary):
     ret = []
-    for AA in list(AA_seqs.keys()):
+    tmind = tmdict[tm]
+    for AA in list(dictionary.keys()):
         index = 0
         while index >= 0:
-            index = AA_seqs[AA][tm].find(seq, index)
+            index = dictionary[AA][tmind].find(seq, index)
             if index >= 0:
                 ret.append(index)
                 index += 1
     return mode(ret)
 
-print(findAA(3, 'aacab'))
+print(find_feature('7', 'aacab', AA_seqs))
 
-def find3Di(tm, seq):
-    ret = []
-    for Di in list(Di_seqs.keys()):
-        index = 0
-        while index >= 0:
-            index = Di_seqs[Di][tm].find(seq, index)
-            if index >= 0:
-                ret.append(index)
-                index += 1
-    return mode(ret)
+print(find_feature('6', 'VVLVV', Di_seqs))
 
-print(find3Di(2, 'VVLVV'))
+
+with open('Feature_Importance/sulfur_importance.txt') as f:
+    lines = f.readlines()
+    for line in lines:
+        line = line.replace('\n', "")
+        if 'TM' in line:
+            if line[0].isupper():
+                find_feature(line[-1], line[0:5], Di_seqs)
+            else:
+                find_feature(line[-1], line[0:5], AA_seqs)
