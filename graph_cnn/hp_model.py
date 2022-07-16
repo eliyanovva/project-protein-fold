799d8028d7bbc0ced748868c6381eb08695dd485
Teodora Petkova <teodorapetkovax@gmail.com>
2022-07-15 22:34:59 -0400

    added graph cnn cli argument parsing

---
+++ graph_cnn/hp_model.py
@@ -0,0 +1,111 @@
+#import os
+#import sys
+
+# FIXME: VERY UGLY FIX FOR THE config.py
+# If the file is called through entry_points, this can be removed.
+#MAIN_PACKAGE_DIR = os.path.abspath(os.curdir)
+#last_dir = os.path.split(os.curdir)[-1]
+#while last_dir != "project-protein-fold":
+#    os.chdir("..")
+#    MAIN_PACKAGE_DIR = os.path.abspath(os.curdir)
+#    last_dir = os.path.split(MAIN_PACKAGE_DIR)[-1]
+    
+#sys.path.append(MAIN_PACKAGE_DIR)
+
+from graph_cnn.model import GraphCNN
+import graph_cnn.run_model as run_model
+import time
+import tensorflow as tf
+import config
+from tensorboard.plugins.hparams import api as hp
+import logging as log
+
+
+with tf.summary.create_file_writer('logs/hparam_tuning').as_default():
+    hp.hparams_config(
+        hparams=[config.HP_BATCH_SIZE, config.HP_DROPOUT, config.HP_OPTIMIZER],
+        metrics=[hp.Metric(config.METRIC_ACCURACY, display_name='Accuracy')],
+    )
+
+def hpBuildModel(hparams = {
+                #config.HP_NUM_UNITS: num_units,
+                #config.HP_DROPOUT: dropout_rate,
+                config.HP_BATCH_SIZE: 32,
+                config.HP_OPTIMIZER: 'adagrad',
+            }):
+    g = GraphCNN()
+    g.initialize()
+
+    start_train_test_split = time.time()
+    X_train_labels, X_test_labels, y_train_labels, y_test_labels = g.trainTestSplit()
+    end_train_test_split = time.time()
+
+    start_train_data_load = time.time()
+    X_train, y_train = g.getTensors(X_train_labels, y_train_labels)
+    end_train_data_load = time.time()
+
+    start_test_data_load = time.time()
+    X_test, y_test = g.getTensors(X_test_labels, y_test_labels)
+    end_test_data_load = time.time()
+
+    callbacks = run_model.createCallbacks()
+
+    start_model_fitting = time.time()
+    model = g.createModel(hparams[config.HP_OPTIMIZER])
+    log.info('model fitting started')
+    #look into ideal batch size
+    mod_history = model.fit(X_train, y_train, epochs=10, verbose=True, batch_size=hparams[config.HP_BATCH_SIZE], callbacks = callbacks, validation_split=0.2)
+    end_model_fitting = time.time()
+
+    log.info ('model fitting finished successfully')
+
+    timing_measures = [
+        end_train_test_split - start_train_test_split,
+        end_train_data_load - start_train_data_load,
+        end_test_data_load - start_test_data_load,
+        end_model_fitting - start_model_fitting,
+        end_model_fitting - start_train_test_split
+    ]
+
+    log.info('model evaluation started')
+    with open('hp_results.txt', 'a') as res_log:
+        results = model.evaluate(X_test, y_test, verbose=True)
+        res_log.write(' '.join([str(r) for r in results]) + ' \n')
+        res_log.write('Timing Benchmarks:\n')
+        res_log.write(' '.join([str(r) for r in timing_measures]) + '\n')
+
+    print(results)
+    log.info('model evaluation completed')
+    #returns loss value (MeanSquaredLogarithmicError) and metric values, currently LogCoshError and coeff_determination
+    #and RootMeanSquaredError and MeanSquaredError
+    #run_model.createPlot(mod_history)
+    return results[0]
+
+
+def hpRunModel(run_dir, hparams):
+  with tf.summary.create_file_writer(run_dir).as_default():
+    hp.hparams(hparams)  # record the values used in this trial
+    accuracy = hpBuildModel(hparams)
+    tf.summary.scalar(config.METRIC_ACCURACY, accuracy, step=1)
+
+
+def optimizeHyperparameters():
+    session_num = 0
+
+#    for num_units in config.HP_NUM_UNITS.domain.values:
+#        for dropout_rate in (config.HP_DROPOUT.domain.min_value, config.HP_DROPOUT.domain.max_value):
+    for batch_size in config.HP_BATCH_SIZE.domain.values:
+        for optimizer in config.HP_OPTIMIZER.domain.values:
+            hparams = {
+                #config.HP_NUM_UNITS: num_units,
+                #config.HP_DROPOUT: dropout_rate,
+                config.HP_BATCH_SIZE: batch_size,
+                config.HP_OPTIMIZER: optimizer,
+            }
+            run_name = "run-%d" % session_num
+            print('--- Starting trial: %s' % run_name)
+            print({h.name: hparams[h] for h in hparams})
+            hpRunModel('logs/hparam_tuning/' + run_name, hparams)
+            session_num += 1
+
+#optimizeHyperparameters()
---
+++ graph_cnn/run_model.py
@@ -0,0 +1,126 @@
+import time
+from graph_cnn.model import GraphCNN
+import config
+import logging as log
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import visuals
+
+
+def createPlot(mod_history):
+    #plot the loss curve: test vs training
+    fig, ax1 = plt.subplots(1, figsize=(15, 5))
+
+    ax1.plot(mod_history.history["loss"])
+    ax1.plot(mod_history.history["val_loss"])
+    ax1.legend(["train", "test"], loc="upper right")
+    ax1.set_xlabel("Epochs")
+    ax1.set_ylabel("Loss")
+
+    plt.savefig('visuals/loss_graph.png')
+    plt.close()
+
+
+def createScatterPlot(mod_history):
+    # Plot the results
+    #print(mod_history.history.keys())
+    #acc = mod_history.history['accuracy']
+    loss = mod_history.history['loss']
+    val_loss = mod_history.history['val_loss']
+    epochs = range(len(loss))
+
+    plt.scatter(epochs[1:], loss[1:], c='red', label='Training MSE')
+    plt.scatter(epochs[1:], val_loss[1:], c='blue', label='Validation MSE')
+    plt.title('Training accuracy')
+    plt.legend(loc=0)
+
+    plt.savefig('visuals/training_accuracy.png')
+    plt.close()
+
+
+def createCallbacks():
+    callbacks = [
+        tf.keras.callbacks.EarlyStopping(
+            monitor = 'val_loss',
+            patience = 0,
+            restore_best_weights = True,
+        )
+    ]
+
+    return callbacks
+
+
+def trainGNN(gnn, X_train, y_train, tr_batch_size=32, tr_optimizer='adagrad'):
+    model = gnn.createModel(hp_optimizer=tr_optimizer)
+    callbacks = createCallbacks()
+
+    log.info('model fitting started')
+    mod_history = model.fit(
+        X_train, y_train, epochs=10, verbose=True,
+        batch_size=tr_batch_size, callbacks = callbacks, validation_split=0.2)
+    
+    log.info ('model fitting finished successfully')
+
+    return model, mod_history
+
+
+def testGNN(model, X_test, y_test):
+    log.info('model evaluation started')
+    results = model.evaluate(X_test, y_test, verbose=True)
+    log.info('The results of the test run are:' + ' '.join([str(r) for r in results]))
+    log.info('model evaluation completed')
+    return results
+
+
+def storeResults(results, timing_measures):
+    with open('results.txt', 'a') as res_log:
+        res_log.write(' '.join([str(r) for r in results]) + ' \n')
+        res_log.write('Timing Benchmarks:\n')
+        res_log.write(' '.join([str(r) for r in timing_measures]) + '\n')
+
+
+def evaluateTuple(model, X_val):
+    """Should predict the binding coefficient of a new tuple.
+
+    Args:
+        model (_type_): _description_
+        X_val (_type_): _description_
+    """
+    pass
+
+
+def runModel(batch_size=-1):
+    gnn = GraphCNN()
+    gnn.initialize()
+    
+    start_train_test_split = time.time()
+    X_train_labels, X_test_labels, y_train_labels, y_test_labels = gnn.trainTestSplit(batch_size=batch_size)
+    end_train_test_split = time.time()
+
+    start_train_data_load = time.time()
+    X_train, y_train = gnn.getTensors(X_train_labels, y_train_labels)
+    end_train_data_load = time.time()
+
+    start_test_data_load = time.time()
+    X_test, y_test = gnn.getTensors(X_test_labels, y_test_labels)
+    end_test_data_load = time.time()
+
+    start_model_fitting = time.time()
+    model, training_history = trainGNN(gnn, X_train, y_train)
+    end_model_fitting = time.time()
+
+    start_model_fitting = time.time()
+    results = testGNN(model, X_test, y_test)
+    end_model_fitting = time.time()
+
+    timing_measures = [
+        end_train_test_split - start_train_test_split,
+        end_train_data_load - start_train_data_load,
+        end_test_data_load - start_test_data_load,
+        end_model_fitting - start_model_fitting,
+        end_model_fitting - start_train_test_split
+    ]
+
+    storeResults(results, timing_measures)
+
+#runModel()
