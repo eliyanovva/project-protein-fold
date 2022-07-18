13146128ba2865d175b056942c35b6ce12f4dbfa
Teodora Petkova <teodorapetkovax@gmail.com>
2022-07-18 16:06:27 -0400

    added docstrings to the gnn model

--- graph_cnn/model.py
+++ graph_cnn/model.py
@@ -1,7 +1,12 @@
+"""This module contains the Graph Neural Network class. It accepts the graph representations
+of a protein and a ligand to make a real number prediction of their binding affinity. A predition
+greater than 1 means that the protein and ligand are likely to bind.
+"""
+
 import logging as log
-from operator import mod
 import os
 from contextlib import redirect_stdout
+from operator import mod
 
 import config
 import matplotlib.pyplot as plt
@@ -16,11 +21,17 @@ from graph_cnn.protein_handlers import ProteinAdjacencyData, ProteinFeatureData
 log.info(os.getpid())
 
 class GraphCNN:
+    """Graph Neural Network class, based on Tensorflow Convolutional Neural Networks.
+    """
     def __init__(self):
         pass
 
 
     def initialize(self):
+        """Initializes the data handlers for the 4 inputs of the neural network - 
+        a protein adjacency matrix, a protein feature matrix, a ligand adjacency matrix,
+        and a ligand feature matrix.
+        """
         self.prot_adj_data_handler = ProteinAdjacencyData()
         self.prot_feat_data_handler = ProteinFeatureData()
         self.lig_adj_data_handler = LigandAdjacencyData()
@@ -29,18 +40,42 @@ class GraphCNN:
         log.info('initialized GraphCNN class')
 
 
-    def trainTestSplit(self, model_test_size=0.3, batch_size=-1):
-        with open(config.MATRIX_DATA_FILES_PATH + '/uniprot_ligand_logfc_pvalue.csv', 'r') as comb_file:
-            comb_file_lines = comb_file.readlines()
+    def trainTestSplit(
+        self, 
+        model_test_size=0.3,
+        batch_size=-1,
+        prot_lig_target_file=config.MATRIX_DATA_FILES_PATH + '/uniprot_ligand_logfc_pvalue.csv'
+        ):
+        """Splits the data labels into a training and testing set. Used as a first step, since
+        each data point is too large, hence the real data is used later.
 
+        Args:
+            model_test_size (float, optional): The fraction of the dataset that is to be
+            used for testing. Defaults to 0.3.
+            batch_size (int, optional): The number of datapoints from the dataset to be used.
+            If the default value is preserved, the entire set is used. Defaults to -1.
+            prot_lig_target_file (str, optional): The path to the labels of the dataset.
+            Each row in the labels file should start with the following 3 separated by a comma:
+            PROTEIN LABEL, LIGAND LABEL, INTERACTION SCORE.
+
+        Returns:
+            list[list[str]], list[list[str]], list[int], list[int]:
+             - A list of the training labels in format [[protein_label, ligand_label], ...]
+             - A list of the testing labels in format [[protein_label, ligand_label], ...]
+             - A list of the training outputs
+             - A list of the testing outputs
+        
+        """
+        with open(prot_lig_target_file, 'r') as comb_file:
+            comb_file_lines = comb_file.readlines()
             if batch_size == -1:
                 batch_size = len(comb_file_lines)
- 
             x, y = [], []
+        
         for row in comb_file_lines[:batch_size]:
             x_new, y_new = self.__extractRowDataProteinLigandPair(row)
             x.append(x_new)
-            y.append(y_new) # logfc score only; doesn't make sense to predict pvalues
+            y.append(y_new) 
 
         X_train, X_test, y_train, y_test = train_test_split(
             x, y, test_size=model_test_size
@@ -52,6 +87,17 @@ class GraphCNN:
 
 
     def createModel(self, hp_optimizer='adagrad'):
+        """This function conatins the main model architecture. It initializes the data Tensors
+        to be used for training the model, then creates and compiles the model. The real data is
+        NOT accessed by this function.
+
+        Args:
+            hp_optimizer (str, optional): The optimizer to be used for the model.
+            Defaults to 'adagrad'.
+
+        Returns:
+            tf.keras.Model: The neural network model.
+        """
         
         prot_adj_in = tf.keras.layers.Input(
             shape=(config.PROTEIN_ADJACENCY_MAT_SIZE, config.PROTEIN_ADJACENCY_MAT_SIZE),
@@ -131,6 +177,20 @@ class GraphCNN:
 
 
     def getTensors(self, X, y):
+        """Generates data tensors from the training and testing lists of labels.
+
+        Args:
+            X (list[list[str]]): A list of lists with a protein and a ligand label.
+            y (list[int]): A list of binding coefficient values for X.
+
+        Returns:
+            tf.Tensor: The function returns five tensors:
+             - A tensor with all protein adjacency matrices.
+             - A tensor with all protein feature matrices.
+             - A tensor with all ligand adjacency matrices.
+             - A tensor with all ligand feature matrices.
+             - A tensor with all output values.
+        """
         X_proteins = [row[0] for row in X]
         X_ligands = [row[1] for row in X]
 
@@ -181,7 +241,7 @@ class GraphCNN:
         logfc = None
 
         row = row[:-1].split(',')
-        if len(row) == 4:
+        if len(row) >= 3:
             protein_ligand_list.append(row[0]) # protein-ligand(file name from Hiro's lab) tuple
             comp_name_index = row[1].rfind('_')
             protein_ligand_list.append(row[1][comp_name_index + 1:])
