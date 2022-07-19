32aafbe4d9166d01a662d71da420fa36e6bc5e6b
Sophia Leeman <sml96@cs.duke.edu>
2022-07-19 16:11:46 -0400

    New time functions

--- RF/Feature_Importance/Important_Residues.py
+++ RF/Feature_Importance/Important_Residues.py
@@ -1,8 +1,8 @@
 import pandas as pd
 
-with open('Residue_Location.csv', 'w') as f:
+with open('Sulfur_Residue_Location.csv', 'w') as f:
     print('gene,start,sequence', file = f)
-    with open('Aligned_Location.txt') as w:
+    with open('Sulfur_Aligned_Location.txt') as w:
         lines = w.readlines()
         for line in lines:
             line = line.replace('\n', '')
