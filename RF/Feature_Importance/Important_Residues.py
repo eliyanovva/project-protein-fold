2626807de1f345e8c4ad334874cecc79ad91cb0c
Sophia Leeman <sml96@cs.duke.edu>
2022-07-19 11:51:26 -0400

    Found locations and sequences of most important feature in all proteins

--- RF/Feature_Importance/Important_Residues.py
+++ RF/Feature_Importance/Important_Residues.py
@@ -1,12 +1,11 @@
 import pandas as pd
 
 with open('Residue_Location.csv', 'w') as f:
-    df = pd.read_csv('../../data_files/TMdomains/TM.csv')
-    print('accession,start,stop,sequence', file = f)
-    for i in range(len(df)):
-        accession = df.loc[i, 'protein']
-        sequence = df.loc[i, 'TM6']
-        start = int(df.loc[i, 's6']) + 18
-        stop = int(df.loc[i, 's6']) + 27
-        sequence = sequence[18:28]
-        print(accession + ',' + str(start) + ',' + str(stop) + ',' + sequence, file = f)
+    print('gene,start,sequence', file = f)
+    with open('Aligned_Location.txt') as w:
+        lines = w.readlines()
+        for line in lines:
+            line = line.replace('\n', '')
+            line = line[0:9] + ',' + line[9:12] + ',' + line[12:]
+            line = line.replace(" ", "")
+            print(line, file = f)
