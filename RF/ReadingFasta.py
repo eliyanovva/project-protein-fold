c1f751b94894bbf0561b14c083fac5842253cc80
Molly Borowiak <molly.borowiak@duke.edu>
2022-07-26 12:19:37 -0400

    update style of docstrings in ReadingFasta.py

--- RF/ReadingFasta.py
+++ RF/ReadingFasta.py
@@ -8,14 +8,14 @@ def make_seqvar_TMS(TM_dict, TM_num, k):
     """
     This function extracts a kmer frequency dictionary for a single transmembrane (TM) domain
     Args:
-        TM_dict (dict): key = (string) id,
-            value = (list) [sequence for TM3, sequence for TM5, sequence for TM6, sequence for TM7]
+        TM_dict (dict): a dictionary mapping a protein id to a list of input sequences for each TM
+            ex: TM_dict[id] = [sequence for TM3, sequence for TM5, sequence for TM6, sequence for TM7]
         TM_num (int): index to extract desired TM
         k (int): k (int): desired kmer length
 
     Returns:
-        seqvar (dict): key = (string) id,
-            value = (dict) key = (string) kmer, value = (int) freq. of kmer in id
+        seqvar (dict): dictionary mapping a protein id to a frequency dictionary
+            ex: seqvar[id]: key = (str) kmer, value = (int) freq. of kmer in id
     """
     seqvar = {}
     feat = set()
@@ -30,12 +30,12 @@ def featurize(seq,k,feat):
     """
     This function extracts kmers and kmer frequencies from seq
     Args:
-        seq (string): sequence of amino acids or 3di input
+        seq (str): sequence of amino acids or 3di input
         k (int): desired kmer length
         feat (set): set of kmers found in previous calls to featurize
 
     Returns:
-        freq_dict (dict): key = (string) kmer, value = (int) freq. of kmer in seq
+        freq_dict (dict): dictionary mapping a (str) kmer to the (int) freq. of kmer in seq
     """
     freq_dict = {}
 
@@ -57,17 +57,17 @@ def makematrix(seqvar, feat, mat, unique_l, counts):
     """
     This function creates a 2-D matrix of frequency values
     Args:
-        seqvar (dict): key = (string) id,
-            value = (dict) key = (string) kmer, value = (int) freq. of kmer in id
+        seqvar (dict): dictionary mapping a protein id to a frequency dictionary
+            ex: seqvar[id]: key = (str) kmer, value = (int) freq. of kmer in id
         feat (list): list of kmers for all the protein ids in seqvar
         mat (list): matrix to add the frequency values to; can be empty or already have values
         unique_l (list): list of unique ligands to use in the final matrix
-        counts (dict): key = (string) id, value = (list) list of ligand that the protein can pair with
+        counts (dict): dictionary mapping a protein id to the list of ligands that id can interact with
             counts will refer exclusively either to positive or negative pairs
 
     Returns:
         mat (list) = matrix of frequency values
-            mat[i][j] = freq. of kmer j in protein i
+            ex: mat[i][j] = freq. of kmer j in protein i
     """
     for id in counts:
         newseq = []
